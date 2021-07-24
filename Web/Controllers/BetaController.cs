5612094d1d8b6fdc01856c33e9b8f8d5bbf338a5
Michal Blaha <michal@michalblaha.cz>
2021-07-24 16:47:36 +0200

    tmp

--- Web/Controllers/BetaController.cs
+++ Web/Controllers/BetaController.cs
@@ -29,6 +29,59 @@ namespace HlidacStatu.Web.Controllers
             return Json(searchResult.Select(r => r.Original));
         }
 
+     
+        public JsonResult Autocomplete2(string q)
+        {
+            var searchCache = StaticData.FulltextSearchForAutocomplete.Get();
+
+            IEnumerable<Entities.Autocomplete> searchResult = Newtonsoft.Json.JsonConvert.DeserializeObject<Entities.Autocomplete[]>(_asdf);
+
+            return Json(searchResult);
+        }
+
+        static string _asdf = @"[
+  {
+    ""id"": ""ico:00006947"",
+    ""text"": ""Ministerstvo financí"",
+    ""imageElement"": ""fas fa-university"",
+    ""type"": ""úřad"",
+    ""description"": ""Hlavní město Praha"",
+    ""priority"": 2
+  },
+  {
+    ""id"": ""ico:28569113"",
+    ""text"": ""MINORR FINANCE a.s."",
+    ""imageElement"": ""fas fa-industry-alt"",
+    ""type"": ""firma"",
+    ""description"": ""Moravskoslezský kraj"",
+    ""priority"": 0
+  },
+  {
+    ""id"": ""ico:27415414"",
+    ""text"": ""MINT Financial Services, s.r.o., v likvidaci"",
+    ""imageElement"": ""fas fa-industry-alt"",
+    ""type"": ""firma"",
+    ""description"": ""Hlavní město Praha"",
+    ""priority"": 0
+  },
+  {
+    ""id"": ""ico:48137430"",
+    ""text"": ""Ministerstvo financí České republiky Generální ředitelství cel"",
+    ""imageElement"": ""fas fa-industry-alt"",
+    ""type"": ""firma"",
+    ""description"": """",
+    ""priority"": 0
+  },
+  {
+    ""id"": ""ico:00001376"",
+    ""text"": ""FEDERÁLNÍ MINISTERSTVO FINANCÍ"",
+    ""imageElement"": ""fas fa-industry-alt"",
+    ""type"": ""firma"",
+    ""description"": """",
+    ""priority"": 0
+  }
+]";
+
         public ActionResult FiveHundred()
         {
             int result = 0;
