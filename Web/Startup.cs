0b1d064bd97d2df8b44431a2568a7a7cf6228ecf
Michal Blaha <michal@michalblaha.cz>
2021-08-10 19:29:51 +0200

    Update Startup.cs

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -220,10 +220,17 @@ namespace HlidacStatu.Web
             //force init autocomplete cache
             new System.Threading.Thread(() =>
             {
-                using (Devmasters.Net.HttpClient.URLContent net = new Devmasters.Net.HttpClient.URLContent("https://www.hlidacstatu.cz/api/autocomplete/?q=flakan&term=flakan&_type=query&q=flakan"))
+                try
                 {
-                    net.Timeout = 3 * 60000;
-                    var s = net.GetContent();
+                    using (Devmasters.Net.HttpClient.URLContent net = new Devmasters.Net.HttpClient.URLContent("https://www.hlidacstatu.cz/api/autocomplete/?q=flakan&term=flakan&_type=query&q=flakan"))
+                    {
+                        net.Timeout = 3 * 60000;
+                        var s = net.GetContent();
+                    }
+
+                }
+                catch (Exception)
+                {
                 }
             }).Start();
 
