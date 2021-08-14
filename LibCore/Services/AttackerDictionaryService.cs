d4795352e0e936a2a58cadbeba2745d3c1b57d39
Michal Blaha <michal@michalblaha.cz>
2021-08-14 14:13:52 +0200

    Update AttackerDictionaryService.cs

--- LibCore/Services/AttackerDictionaryService.cs
+++ LibCore/Services/AttackerDictionaryService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
@@ -25,6 +26,7 @@ namespace HlidacStatu.LibCore.Services
 
         const int SaveTimeBetweentAttacksInSec = 15*60; //inSec
         const int PenaltyLimit = 1500; //inSec
+        static string[] whitelistedIps = new string[] { "77.93.208.131", "217.31.202.16", "89.22.68.163" };
 
         public bool IsAttacker(IPAddress? ipAddress, int statusCode, string? path)
         {
@@ -32,7 +34,7 @@ namespace HlidacStatu.LibCore.Services
                 return false;
 
             var ipString = IpToString(ipAddress);
-            if (ipString.StartsWith("10.10") || ipString == "77.93.208.131")
+            if (ipString.StartsWith("10.10") || whitelistedIps.Contains(ipString ) )
                 return false;
             
             var currentTime = DateTime.Now;
