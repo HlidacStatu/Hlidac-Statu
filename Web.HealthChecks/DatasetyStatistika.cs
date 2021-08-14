7e42c1019575dc872e06005b6ba875bbeed70949
Michal Blaha <michal@michalblaha.cz>
2021-08-14 12:23:10 +0200

    Update DatasetyStatistika.cs

--- Web.HealthChecks/DatasetyStatistika.cs
+++ Web.HealthChecks/DatasetyStatistika.cs
@@ -121,11 +121,16 @@ namespace HlidacStatu.Web.HealthChecks
             {
                 var data = statistiky.Get();
                 System.Text.StringBuilder sb = new System.Text.StringBuilder(1024);
-
+                sb.Append("<table class='table table-new table-dotted'>");
+                sb.Append($"<thead><tr><td>dataset</td><td>celkem zaznamu</td><td>za {options.Interval.ToNiceDisplayName()}</td><td>posl.změna</td></tr></thead>");
+                sb.Append("<tbody>");
                 foreach (var ds in data.Where(ds=>options.Exclude.Contains(ds.Dataset.DatasetId)==false))
                 {
-                    sb.AppendLine($"{ds.Dataset.DatasetId}: {ds.PocetCelkem} celkem, {ds.PosledniZmena:dd.MM.yy} poslední změna, {ds.PocetZaInterval[options.Interval]} za {options.Interval.ToNiceDisplayName()}");
+                    sb.Append("<tr>");
+                    sb.Append($"<td>{ds.Dataset.DatasetId}</td><td>{ds.PocetCelkem}</td><td>{ds.PocetZaInterval[options.Interval]}</td><td>{ds.PosledniZmena:dd.MM.yy}</td>");
+                    sb.Append("</tr>");
                 }
+                sb.Append("</tbody></table>");
 
                 return Task.FromResult(HealthCheckResult.Healthy(sb.ToString()));
             }
