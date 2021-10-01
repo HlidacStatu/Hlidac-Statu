cdeee24481f230aabf71142a525a5a2516361df0
suchoss <suchoss@gmail.com>
2021-10-01 10:57:03 +0200

    Jobs web skeleton

---
+++ HlidacStatu.JobsWeb/Startup.cs
@@ -0,0 +1,95 @@
+using System;
+using HlidacStatu.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HlidacStatu.JobsWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Devmasters.Config.Init(Configuration);
+
+            System.Globalization.CultureInfo.DefaultThreadCurrentCulture = Util.Consts.czCulture;
+            System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = Util.Consts.csCulture;
+
+            
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            // for scoped services (mainly for identity)
+            services.AddDbContext<DbEntities>(options =>
+                options.UseSqlServer(connectionString));
+            
+            AddIdentity(services);
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
+        }
+        
+        private void AddIdentity(IServiceCollection services)
+        {
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+
+                    options.User.RequireUniqueEmail = true;
+
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                    options.Lockout.MaxFailedAccessAttempts = 5;
+
+                })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<DbEntities>();
+
+            // this is needed because passwords are stored with old hashes
+            services.Configure<PasswordHasherOptions>(options =>
+                options.CompatibilityMode = PasswordHasherCompatibilityMode.IdentityV2
+            );
+        }
+    }
+}
