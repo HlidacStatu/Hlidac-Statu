dc4f4389edec427cc29b53850f054400288cae91
suchoss <suchoss@gmail.com>
2021-10-05 14:40:17 +0200

    Some cleanup, adding recalculate page

---
+++ HlidacStatu.JobsWeb/Pages/Recalculate.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using HlidacStatu.JobsWeb.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HlidacStatu.JobsWeb.Pages
+{
+    public class RecalculateModel : PageModel
+    {
+        
+        public async Task OnPostAsync()
+        {
+            if(!JobService.IsRecalculating)
+                await JobService.Recalculate();
+        }
+    }
+}
--- HlidacStatu.JobsWeb/Services/JobService.cs
+++ HlidacStatu.JobsWeb/Services/JobService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using HlidacStatu.JobsWeb.Models;
@@ -12,7 +13,7 @@ namespace HlidacStatu.JobsWeb.Services
     {
         
         // make it "in memory", load it asynchronously, recalculate once a day?
-        private static List<JobPrecalculated> DistinctJobs { get; set; }
+        //private static List<JobPrecalculated> DistinctJobs { get; set; }
         
         private static List<JobStatistics> JobOverview { get; set; }
         
@@ -20,41 +21,82 @@ namespace HlidacStatu.JobsWeb.Services
         
         private static Dictionary<string, List<JobStatistics>> OdberatelOverview { get; set; }
         private static Dictionary<string, List<JobStatistics>> DodavatelOverview { get; set; }
+        
+        //Meta informations
+        public static DateTime LastRecalculationStarted { get; private set; }
+        public static DateTime JobRecalculationEnd { get; private set; }
+        public static DateTime TagRecalculationEnd { get; private set; }
+        public static DateTime OdberateleRecalculationEnd { get; private set; }
+        public static DateTime DodavateleRecalculationEnd { get; private set; }
+        public static long RecalculationTimeMs { get; private set; }
+        
+        public static bool IsRecalculating { get; private set; }
+        public static string LastError { get; private set; } 
 
 
-        public static async Task LoadData()
+        public static async Task Recalculate()
         {
-            var allJobs = await InDocJobsRepo.GetAllJobsWithRelatedDataAsync();
+            IsRecalculating = true;
+            LastRecalculationStarted = DateTime.Now;
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            try
+            {
+                LastError = "";
+                var allJobs = await InDocJobsRepo.GetAllJobsWithRelatedDataAsync();
             
-            // important to filter duplicates here, can be extended in future
-            DistinctJobs = allJobs
-                .GroupBy(j => j.JobPk)
-                .Select(g =>
-                {
-                    var (net, vat) = FixSalaries(g.FirstOrDefault().SalaryMd, g.FirstOrDefault().SalaryMdVat);
-                    return new JobPrecalculated()
+                // important to filter duplicates here, can be extended in future
+                var distinctJobs = allJobs
+                    .GroupBy(j => j.JobPk)
+                    .Select(g =>
                     {
-                        Subject = g.FirstOrDefault().Subject,
-                        Year = g.FirstOrDefault().Year,
-                        IcoOdberatele = g.FirstOrDefault().IcoOdberatele,
-                        JobGrouped = g.FirstOrDefault().JobGrouped,
-                        SmlouvaId = g.FirstOrDefault().SmlouvaId,
-                        Tags = g.FirstOrDefault().Tags?
-                            .Split("|", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries) 
-                               ?? new []{"-"},
-                        JobPk = g.Key,
-                        SalaryMd = net,
-                        SalaryMdVat = vat,
-                        IcoDodavatele = g.Select(i => i.IcoOdberatele).ToArray(),
-                    };
-                }).ToList();
+                        var (net, vat) = FixSalaries(g.FirstOrDefault().SalaryMd, g.FirstOrDefault().SalaryMdVat);
+                        return new JobPrecalculated()
+                        {
+                            Subject = g.FirstOrDefault().Subject,
+                            Year = g.FirstOrDefault().Year,
+                            IcoOdberatele = g.FirstOrDefault().IcoOdberatele,
+                            JobGrouped = g.FirstOrDefault().JobGrouped,
+                            SmlouvaId = g.FirstOrDefault().SmlouvaId,
+                            Tags = g.FirstOrDefault().Tags?
+                                       .Split("|", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries) 
+                                   ?? new []{"-"},
+                            JobPk = g.Key,
+                            SalaryMd = net,
+                            SalaryMdVat = vat,
+                            IcoDodavatele = g.Select(i => i.IcoOdberatele).ToArray(),
+                        };
+                    }).ToList();
             
-            JobOverview = DistinctJobs
+                CalculateJobs(distinctJobs);
+
+                CalculateTags(distinctJobs);
+            
+                CalculateOdberatele(distinctJobs);
+            
+                CalculateDodavatele(distinctJobs);
+            
+            }
+            catch (Exception e)
+            {
+                LastError = e.Message;
+            }
+            
+            stopWatch.Stop();
+            RecalculationTimeMs = stopWatch.ElapsedMilliseconds;
+            IsRecalculating = false;
+        }
+
+        private static void CalculateJobs(List<JobPrecalculated> distinctJobs)
+        {
+            JobOverview = distinctJobs
                 .GroupBy(j => j.JobGrouped)
                 .Select(g =>
                 {
                     var salaryd = g.Select(x => (double)x.SalaryMd).ToList();
-                    
+
                     return new JobStatistics()
                     {
                         Name = g.Key,
@@ -65,16 +107,19 @@ namespace HlidacStatu.JobsWeb.Services
                         DolniKvartil = (decimal)salaryd.LowerQuartile(),
                         HorniKvartil = (decimal)salaryd.UpperQuartile(),
                         ContractCount = g.Count(),
-                        SupplierCount = g.Select(x=>x.IcoOdberatele).Count()
-
+                        SupplierCount = g.Select(x => x.IcoOdberatele).Count()
                     };
                 })
                 .ToList();
+            JobRecalculationEnd = DateTime.Now;
+        }
 
-            TagOverview = DistinctJobs
+        private static void CalculateTags(List<JobPrecalculated> distinctJobs)
+        {
+            TagOverview = distinctJobs
                 .SelectMany(j => j.Tags, (precalculated, tag) => new { precalculated, dodavatel = tag })
                 .GroupBy(j => j.precalculated.JobGrouped)
-                .ToDictionary(g => g.Key , g =>
+                .ToDictionary(g => g.Key, g =>
                     g.GroupBy(i => i.dodavatel)
                         .Select(ig =>
                         {
@@ -91,15 +136,18 @@ namespace HlidacStatu.JobsWeb.Services
                                 HorniKvartil = (decimal)salaryd.UpperQuartile(),
                                 ContractCount = ig.Count(),
                                 SupplierCount = ig.Select(x => x.precalculated.IcoOdberatele).Count()
-
                             };
                         })
                         .ToList()
                 );
-            
-            OdberatelOverview = DistinctJobs
+            TagRecalculationEnd = DateTime.Now;
+        }
+
+        private static void CalculateOdberatele(List<JobPrecalculated> distinctJobs)
+        {
+            OdberatelOverview = distinctJobs
                 .GroupBy(j => j.IcoOdberatele)
-                .ToDictionary(g => g.Key , g =>
+                .ToDictionary(g => g.Key, g =>
                     g.GroupBy(i => i.JobGrouped)
                         .Select(ig =>
                         {
@@ -116,16 +164,19 @@ namespace HlidacStatu.JobsWeb.Services
                                 HorniKvartil = (decimal)salaryd.UpperQuartile(),
                                 ContractCount = ig.Count(),
                                 SupplierCount = ig.Select(x => x.IcoOdberatele).Count()
-
                             };
                         })
                         .ToList()
                 );
-            
-            DodavatelOverview = DistinctJobs
+            OdberateleRecalculationEnd = DateTime.Now;
+        }
+
+        private static void CalculateDodavatele(List<JobPrecalculated> distinctJobs)
+        {
+            DodavatelOverview = distinctJobs
                 .SelectMany(j => j.IcoDodavatele, (precalculated, dodavatel) => new { precalculated, dodavatel })
                 .GroupBy(j => j.dodavatel)
-                .ToDictionary(g => g.Key , g =>
+                .ToDictionary(g => g.Key, g =>
                     g.GroupBy(i => i.precalculated.JobGrouped)
                         .Select(ig =>
                         {
@@ -142,11 +193,11 @@ namespace HlidacStatu.JobsWeb.Services
                                 HorniKvartil = (decimal)salaryd.UpperQuartile(),
                                 ContractCount = ig.Count(),
                                 SupplierCount = ig.Select(x => x.precalculated.IcoOdberatele).Count()
-
                             };
                         })
                         .ToList()
                 );
+            DodavateleRecalculationEnd = DateTime.Now;
         }
 
         private static (decimal net, decimal vat) FixSalaries(decimal? net, decimal? vat)
