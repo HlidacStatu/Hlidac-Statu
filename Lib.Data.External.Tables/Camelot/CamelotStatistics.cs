637a570849cff78e69781343fc7dc4be4a5df636
Michal Blaha <michal@michalblaha.cz>
2021-10-20 13:35:06 +0200

    camelot stats update

--- Lib.Data.External.Tables/Camelot/CamelotStatistics.cs
+++ Lib.Data.External.Tables/Camelot/CamelotStatistics.cs
@@ -5,8 +5,13 @@ namespace HlidacStatu.Lib.Data.External.Tables.Camelot
     public class CamelotStatistics
     {
         public long MaxThreads { get; set; }
-        public long Calls { get; set; }
-        public long ParsedFiles { get; set; }
+        public long CallsTotal { get; set; }
+        public long CallsIn1H { get; set; }
+        public long CallsIn24H { get; set; }
+        public long ParsedFilesTotal { get; set; }
+        public long ParsedFilesIn1H { get; set; }
+        public long ParsedFilesIn24H { get; set; }
+
         public DateTime Started { get; set; }
         public long CurrentThreads { get; set; }
         public long SessionsOnDisk { get; set; }
