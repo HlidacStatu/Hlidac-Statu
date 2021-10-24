7d2ea51bf2290fc6cc37e4c605fe390a37e39e2e
Michal Blaha <michal@michalblaha.cz>
2021-10-25 00:21:04 +0200

    Cached Filtered IDS

---
+++ Repositories/FilteredIdsRepo.cs
@@ -0,0 +1,84 @@
+﻿using HlidacStatu.Entities;
+using HlidacStatu.Util.Cache;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HlidacStatu.Repositories
+{
+    public static class FilteredIdsRepo
+    {
+        public static class CachedIds
+        {
+            public class QueryBatch
+            {
+                public string TaskPrefix { get; set; } = string.Empty;
+
+                public string Query { get; set; }
+                public Action<string> LogOutputFunc { get; set; } = null;
+                public Action<Devmasters.Batch.ActionProgressData> ProgressOutputFunc { get; set; } = null
+
+            }
+
+            private static volatile ElasticCacheManager<string[], QueryBatch> cacheSmlouvy
+                = ElasticCacheManager<string[], QueryBatch>.GetSafeInstance(
+                    "cachedIdsSmlouvy",
+                    q => smlouvy(q),
+                    TimeSpan.FromHours(24),
+                    Devmasters.Config.GetWebConfigValue("ESConnection").Split(';'),
+                    "DevmastersCache", null, null,
+                    key => key.TaskPrefix + Devmasters.Crypto.Hash.ComputeHashToHex(key.Query)
+                    );
+
+            public static string[] Smlouvy(QueryBatch query, bool forceUpdate = false)
+            {
+                if (forceUpdate)
+                {
+                    cacheSmlouvy.Delete(query);
+                }
+
+                return cacheSmlouvy.Get(query);
+
+            }
+
+
+            private static string[] smlouvy(QueryBatch query)
+            {
+                if (query == null)
+                    return new string[] { };
+
+                if (string.IsNullOrEmpty(query.Query))
+                    return new string[] { };
+
+                Func<int, int, Nest.ISearchResponse<Smlouva>> searchFunc = (size, page) =>
+                {
+                    return ES.Manager.GetESClient().Search<Smlouva>(a => a
+                                .Size(size)
+                                .Source(false)
+                                //.Fields(f => f.Field("Id"))
+                                .From(page * size)
+                                .Query(q => SmlouvaRepo.Searching.GetSimpleQuery(query.Query))
+                                //.Sort(s => s.Ascending(ss => ss.LastUpdate))
+                                .Scroll("1m")
+                                );
+                };
+
+                List<string> ids2Process = new List<string>();
+                Repositories.Searching.Tools.DoActionForQuery<Smlouva>(ES.Manager.GetESClient(),
+                    searchFunc, (hit, param) =>
+                    {
+                        ids2Process.Add(hit.Id);
+                        return new Devmasters.Batch.ActionOutputData() { CancelRunning = false, Log = null };
+                    }, null, query.LogOutputFunc, query.ProgressOutputFunc, false,
+                    prefix: "getting ids ");
+
+                return ids2Process.ToArray();
+            }
+        }
+
+    }
+}
+}
---
+++ Util/Cache/ElasticCacheManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HlidacStatu.Util.Cache
+{
+
+    public class ElasticCacheManager<T, Key>
+        : Manager<T, Key, Devmasters.Cache.Elastic.ElasticCache<T>>
+        where T : class
+    {
+        private string indicieName = "";
+        private string accessKey = "";
+        private string secretKey = "";
+        private string[] serversUrl;
+        Func<Key, string> keyValueSelector = null;
+        public ElasticCacheManager(string keyPrefix, Func<Key, T> func, TimeSpan expiration
+            , string[] serversUrl, string indicie, string accessKey=null, string secretKey=null
+            , Func<Key, string> keyValueSelector = null)
+            : base(keyPrefix, func, expiration)
+        {
+            indicieName = indicie;
+            this.serversUrl = serversUrl;
+            this.accessKey = accessKey;
+            this.secretKey = secretKey;
+            this.keyValueSelector = keyValueSelector ?? new Func<Key, string>(k => k.ToString());
+        }
+        protected override Devmasters.Cache.Elastic.ElasticCache<T> getTCacheInstance(Key key, TimeSpan expiration, Func<Key, T> contentFunc)
+        {
+            return new Devmasters.Cache.Elastic.ElasticCache<T>(
+                this.serversUrl, this.indicieName,
+                expiration, keyPrefix + keyValueSelector(key), (o) => contentFunc.Invoke(key)
+            );
+        }
+
+        public static ElasticCacheManager<T, Key> GetSafeInstance(string instanceName, Func<Key, T> func, TimeSpan expiration,
+            string[] serversUrl, string bucketName, string accessKey, string secretKey,
+            Func<Key, string> keyValueSelector = null)
+        {
+            lock (instancesLock)
+            {
+                string instanceFullName = instanceName + "|" + typeof(T).ToString() + "|" + typeof(Key).ToString();
+
+                if (!instances.ContainsKey(instanceFullName))
+                {
+                    instances[instanceFullName] = new ElasticCacheManager<T, Key>(instanceName, func, expiration,
+                        serversUrl, bucketName, accessKey, secretKey,
+                        keyValueSelector: keyValueSelector);
+                }
+                return (ElasticCacheManager<T, Key>)instances[instanceFullName];
+            }
+        }
+
+
+
+    }
+}
