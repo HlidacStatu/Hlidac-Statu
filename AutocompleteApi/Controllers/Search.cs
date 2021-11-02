d9b8c1a7b662ee42d1877571ca25730814f63f3a
suchoss <suchoss@gmail.com>
2021-11-02 21:01:15 +0100

    Autocomplete API

---
+++ AutocompleteApi/Controllers/Search.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HlidacStatu.AutocompleteApi.Services;
+using HlidacStatu.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HlidacStatu.AutocompleteApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SearchController : ControllerBase
+    {
+        private MemoryStoreService _memoryStore;
+
+        public SearchController(MemoryStoreService memoryStore)
+        {
+            _memoryStore = memoryStore;
+        }
+
+        [HttpGet]
+        public IEnumerable<Autocomplete> Autocomplete(string q)
+        {
+            var searchResult = _memoryStore.HlidacFulltextIndex.Search(q, 5, ac => ac.Priority);
+
+            return searchResult.Select(r => r.Original);
+        }
+        
+        [HttpGet]
+        public IEnumerable<Autocomplete> SampleComplete(string q)
+        {
+            var searchResult = _memoryStore.SmallSampleIndex.Search(q, 5, ac => ac.Priority);
+
+            return searchResult.Select(r => r.Original);
+        }
+    
+    }
+}
---
+++ AutocompleteApi/Services/MemoryStoreService.cs
@@ -0,0 +1,133 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using FullTextSearch;
+using HlidacStatu.Entities;
+using HlidacStatu.Repositories;
+
+namespace HlidacStatu.AutocompleteApi.Services
+{
+    public class MemoryStoreService
+    {
+        public Index<Autocomplete> HlidacFulltextIndex { get; private set; }
+
+        private readonly object _fullTextLock = new();
+
+        public Index<Autocomplete> SmallSampleIndex { get; private set; }
+
+        private readonly object _sampleLock = new();
+
+
+        public MemoryStoreService()
+        {
+            //quick load from backups so app can restart fast
+            HlidacFulltextIndex = LoadFromBackup<Autocomplete>(nameof(HlidacFulltextIndex));
+            SmallSampleIndex = LoadFromBackup<Autocomplete>(nameof(SmallSampleIndex));
+
+            GenerateAll();
+        }
+
+        public async Task GenerateAll(CancellationToken cancellationToken = default)
+        {
+            await Task.WhenAll(
+                GenerateHlidacFulltextIndex(cancellationToken),
+                GenerateSmallSampleIndex(cancellationToken)
+            );
+        }
+
+
+        public async Task GenerateHlidacFulltextIndex(CancellationToken cancellationToken = default)
+        {
+            lock (_fullTextLock)
+            {
+                var autocompleteData = AutocompleteRepo.GenerateAutocomplete();
+                HlidacFulltextIndex = new Index<Autocomplete>(autocompleteData);
+            }
+
+            await CreateBackup(nameof(HlidacFulltextIndex), HlidacFulltextIndex, cancellationToken);
+        }
+
+        public async Task GenerateSmallSampleIndex(CancellationToken cancellationToken = default)
+        {
+            string _asdf = @"[
+              {
+                ""id"": ""ico:00006947"",
+                ""text"": ""Ministerstvo financí"",
+                ""imageElement"": ""fas fa-university"",
+                ""type"": ""úřad"",
+                ""description"": ""Hlavní město Praha"",
+                ""priority"": 2
+              },
+              {
+                ""id"": ""ico:28569113"",
+                ""text"": ""MINORR FINANCE a.s."",
+                ""imageElement"": ""fas fa-industry-alt"",
+                ""type"": ""firma"",
+                ""description"": ""Moravskoslezský kraj"",
+                ""priority"": 0
+              },
+              {
+                ""id"": ""ico:27415414"",
+                ""text"": ""MINT Financial Services, s.r.o., v likvidaci"",
+                ""imageElement"": ""fas fa-industry-alt"",
+                ""type"": ""firma"",
+                ""description"": ""Hlavní město Praha"",
+                ""priority"": 0
+              },
+              {
+                ""id"": ""ico:48137430"",
+                ""text"": ""Ministerstvo financí České republiky Generální ředitelství cel"",
+                ""imageElement"": ""fas fa-industry-alt"",
+                ""type"": ""firma"",
+                ""description"": """",
+                ""priority"": 0
+              },
+              {
+                ""id"": ""ico:00001376"",
+                ""text"": ""FEDERÁLNÍ MINISTERSTVO FINANCÍ"",
+                ""imageElement"": ""fas fa-industry-alt"",
+                ""type"": ""firma"",
+                ""description"": """",
+                ""priority"": 0
+              }
+            ]";
+
+            lock (_sampleLock)
+            {
+                var acObject = Newtonsoft.Json.JsonConvert.DeserializeObject<Entities.Autocomplete[]>(_asdf);
+                SmallSampleIndex = new FullTextSearch.Index<Autocomplete>(acObject);
+            }
+
+            await CreateBackup(nameof(SmallSampleIndex), SmallSampleIndex, cancellationToken);
+        }
+
+
+        private async Task CreateBackup<T>(string indexName, Index<T> index, CancellationToken cancellationToken)
+            where T : IEquatable<T>
+        {
+            string filename = CreateFileName(indexName);
+            using var filestream = File.Create(filename);
+            await filestream.WriteAsync(index.Serialize(), cancellationToken);
+        }
+        
+        private Index<T> LoadFromBackup<T>(string indexName)
+            where T : IEquatable<T>
+        {
+            string filename = CreateFileName(indexName);
+            if (File.Exists(filename))
+            {
+                var bytes = File.ReadAllBytes(filename);
+                return Index<T>.Deserialize(bytes);
+            }
+
+            return null;
+        }
+
+        private string CreateFileName(string indexName)
+        {
+            return $"{indexName}.bak";
+        }
+    }
+
+}
