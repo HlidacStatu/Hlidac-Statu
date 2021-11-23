cd6db198d6fefd0961833c5d271f07edd3f82347
Michal Blaha <michal@michalblaha.cz>
2021-11-23 16:23:37 +0100

    error hlaska

---
+++ HlidacStatu.JobsWeb/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HlidacStatu.JobsWeb.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
