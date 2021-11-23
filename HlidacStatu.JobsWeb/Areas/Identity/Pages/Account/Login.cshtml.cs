50ead81765c9d2eaabfb86b324df0575de3ffdf6
Michal Blaha <michal@michalblaha.cz>
2021-11-23 13:47:46 +0100

    UX

--- HlidacStatu.JobsWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HlidacStatu.JobsWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -57,7 +57,7 @@ namespace HlidacStatu.JobsWeb.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -69,7 +69,7 @@ namespace HlidacStatu.JobsWeb.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
