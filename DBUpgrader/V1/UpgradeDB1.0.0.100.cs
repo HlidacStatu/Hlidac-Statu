ad0459e74186a337bebe3ba5c07ffd104a6f14b4
suchoss <suchoss@gmail.com>
2022-01-07 10:24:40 +0100

    unit count to db

---
+++ DBUpgrader/V1/UpgradeDB1.0.0.100.cs
@@ -0,0 +1,23 @@
+﻿using Devmasters.DatabaseUpgrader;
+
+
+namespace HlidacStatu.DBUpgrades
+{
+    public static partial class DBUpgrader
+    {
+        private partial class UpgradeDB
+        {
+            [DatabaseUpgradeMethod("1.0.0.100")]
+            public static void Init_1_0_0_100(IDatabaseUpgrader du)
+            {
+                string sql = @"
+alter table indocjobs add
+    UnitCount decimal(18,9);
+	";
+                
+                
+                du.RunDDLCommands(sql);
+            }
+        }
+    }
+}
