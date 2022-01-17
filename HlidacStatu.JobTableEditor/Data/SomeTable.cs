a11c84204ee8262f4c48d57593da34e9304d700a
suchoss <suchoss@gmail.com>
2022-01-17 17:36:49 +0100

    jobtable editor fixes

--- HlidacStatu.JobTableEditor/Data/SomeTable.cs
+++ HlidacStatu.JobTableEditor/Data/SomeTable.cs
@@ -90,6 +90,7 @@ namespace HlidacStatu.JobTableEditor.Data
                     continue;
                 
                 var job = new InDocJobs();
+                job.TablePk = InDocTable.Pk;
 
                 var textCells = foundValues.Where(v => v.IsTextCell).OrderByDescending(v => v.LetterCount);
                 var numberCells = foundValues.Where(v => v.IsNumberCell).OrderByDescending(v => v.DecimalValue).ToList();
@@ -128,6 +129,7 @@ namespace HlidacStatu.JobTableEditor.Data
 
                 job.UnitCount = 1; // default value for unit count (zatím nevím, jak bych tohle číslo parsoval)
                 
+                
                 jobsList.Add(job);
 
             }
@@ -143,7 +145,7 @@ namespace HlidacStatu.JobTableEditor.Data
         public static Regex WithDphRegex = new Regex(@"(s|vc|vcetne)[\. ]{0,2}(dph)", RegexOptions.Compiled);
         public static Regex WithoutDphRegex = new Regex(@"(bez)[\. ]{0,2}(dph)", RegexOptions.Compiled);
         public static Regex CurrencyRegex = new Regex(@"(kc|eur|korun|czk)", RegexOptions.Compiled);
-        public static Regex UnitRegex = new Regex(@"(/|za| |\b)+(mili|micro|kilo|centi|m|k|c)?(metr|minut|hodin|gram|mesic|litr|min|hod|den|kus|kpl|ks|ha|m|h|g|t|l)(a|u|y)?", RegexOptions.Compiled);
+        public static Regex UnitRegex = new Regex(@"(/|za| |\b)+(mili|micro|kilo|centi|m|k|c)?(metr|minut|hodin|gram|mesic|litr|min|hod|den|kus|kpl|mes|ks|ha|m|h|g|t|l)(a|u|y)?", RegexOptions.Compiled);
 
         public static string ReduceText(this string input)
         {
