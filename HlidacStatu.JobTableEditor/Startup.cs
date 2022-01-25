9d8987afe97ec9370d767769964a6ea3c995c76e
suchoss <suchoss@gmail.com>
2022-01-25 11:59:34 +0100

    Fix toasts displaying for all

--- HlidacStatu.JobTableEditor/Startup.cs
+++ HlidacStatu.JobTableEditor/Startup.cs
@@ -47,7 +47,7 @@ namespace HlidacStatu.JobTableEditor
 
 
             services.AddSingleton<JobService>();
-            services.AddSingleton<ToastService>();
+            services.AddScoped<ToastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
