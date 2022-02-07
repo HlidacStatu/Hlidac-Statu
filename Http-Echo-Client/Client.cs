4a851b3485689567abf34ff70b0a3836c3397eac
Michal <michal@michalblaha.cz>
2022-02-07 11:46:16 +0100

    http echo client

---
+++ Http-Echo-Client/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Http_Echo
+{
+    public partial class Client
+    {
+        public static async Task<string> MyIPAddressAsync(Uri echoServerAddress, bool noException = true)
+        {
+            try
+            {
+                Response res= await Devmasters.Net.HttpClient.Simple.GetAsync<Response>(echoServerAddress.ToString());
+                //  "ip": "::ffff:10.10.100.147",
+                string ip = Devmasters.RegexUtil.GetRegexGroupValue(res.ip, @"(?<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", "ip");
+
+                return ip;
+
+            }
+            catch (Exception e)
+            {
+                if (noException)
+                    return "";
+                throw;
+            }
+            
+        }
+
+    }
+}
---
+++ Http-Echo-Client/Response.cs
@@ -0,0 +1,48 @@
+﻿namespace Http_Echo
+{
+    public partial class Client
+    {
+
+        public class Response
+        {
+
+            public string path { get; set; }
+            public Headers headers { get; set; }
+            public string method { get; set; }
+            public string body { get; set; }
+            public bool fresh { get; set; }
+            public string hostname { get; set; }
+            public string ip { get; set; }
+            public string[] ips { get; set; }
+            public string protocol { get; set; }
+            public Query query { get; set; }
+            public string[] subdomains { get; set; }
+            public bool xhr { get; set; }
+            public Os os { get; set; }
+            public Connection connection { get; set; }
+
+            public class Headers
+            {
+                public string host { get; set; }
+                public string useragent { get; set; }
+                public string accept { get; set; }
+            }
+
+            public class Query
+            {
+            }
+
+            public class Os
+            {
+                public string hostname { get; set; }
+            }
+
+            public class Connection
+            {
+            }
+
+        }
+
+
+    }
+}
