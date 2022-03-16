ccbdb10ec48dc93b49299b93147e52b7a5746ea1
Michal <michal@michalblaha.cz>
2022-03-16 19:43:54 +0100

    jobs new HP

---
+++ HlidacStatu.JobsWeb/Services/JobService.AnalysisStat.cs
@@ -0,0 +1,33 @@
+﻿using HlidacStatu.JobsWeb.Models;
+
+using System.Linq;
+
+namespace HlidacStatu.JobsWeb.Services
+{
+    public static partial class JobService
+    {
+        public class AnalysisStat
+        {
+            public AnalysisStat(YearlyStatisticsGroup.Key oblast)
+            {
+                var stat = JobService.GetStatistics(oblast);
+
+                var distinctJobsForYearAndSubject = JobService.DistinctJobsForYearAndSubject(oblast);
+                PocetSmluv = distinctJobsForYearAndSubject
+                    .Select(m => m.SmlouvaId).Distinct().Count();
+                PocetZadavatelu = distinctJobsForYearAndSubject
+                    .Select(m => m.IcoOdberatele).Distinct().Count();
+                PocetDodavatelu = distinctJobsForYearAndSubject
+                    .SelectMany(m => m.IcaDodavatelu).Distinct().Count();
+                PocetCen = distinctJobsForYearAndSubject
+                    .Count();
+
+            }
+
+            public int PocetSmluv { get; }
+            public int PocetZadavatelu { get; }
+            public int PocetDodavatelu { get; }
+            public int PocetCen { get; }
+        }
+    }
+}
