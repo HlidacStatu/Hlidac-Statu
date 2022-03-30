1b42ccf92578c684bd6aa7a1b42539c742eca8cd
suchoss <suchoss@gmail.com>
2022-03-30 13:57:44 +0200

    Fix demo scripts for jobs

--- HlidacStatu.JobsWeb/PrepareAnalysis/migrace DEMO report.sql
+++ HlidacStatu.JobsWeb/PrepareAnalysis/migrace DEMO report.sql
@@ -1,4 +1,4 @@
-
+-- insert mandays
 INSERT INTO Ceny([JobPK]
            ,[SmlouvaId]
            ,[IcoOdberatele]
@@ -8,7 +8,6 @@ INSERT INTO Ceny([JobPK]
            ,[Tags]
            ,[Unit]
            ,[UnitText]
-           ,[PricePerUnit]
            ,[PricePerUnitVAT]
            ,[Year]
            ,[AnalyzaName]
@@ -24,12 +23,10 @@ SELECT
 	iif(j.tags='',null, j.tags) as tags,
 	2 as unit, --MD from enum MeasureUnit
 	'MD' as unitText,
-	iif(j.UnitCount>1, j.salaryMD/j.unitcount,j.salaryMD) as pricePerUnit,
-	iif(j.UnitCount>1, j.salaryMDVat/j.unitcount,j.salaryMDVat)as pricePerUnitVAT,
+    j.PriceVATCalculated as pricePerUnitVAT,
 	t.year,
     'DEMO' as AnalyzaName ,
 	j.created
-
 
 FROM InDocTables t
     join InDocJobs j on t.pk = j.tablePK
@@ -41,7 +38,51 @@ where
 
 	and t.year=2018
 	and j.jobGrouped is not null and j.jobGrouped != '0'
-	and j.salaryMDVat is not null
+	and j.PriceVATCalculated is not null
 	and (j.pk not in (select jobpk from Ceny))
+    and Unit = 2 --manday
 
+--insert manhours as mandays
+INSERT INTO Ceny([JobPK]
+    ,[SmlouvaId]
+    ,[IcoOdberatele]
+    ,[IcoDodavatele]
+    ,[TablePk]
+    ,[Polozka]
+    ,[Tags]
+    ,[Unit]
+    ,[UnitText]
+    ,[PricePerUnitVAT]
+    ,[Year]
+    ,[AnalyzaName]
+    ,[Created])
+
+SELECT
+    j.pk as JobPk,
+    t.smlouvaID,
+    s.IcoOdberatele,
+    d.Ico as IcoDodavatele,
+    j.tablePK,
+    j.jobGrouped as polozka,
+    iif(j.tags='',null, j.tags) as tags,
+    2 as unit, --MD from enum MeasureUnit
+    'MD' as unitText,
+    j.PriceVATCalculated * 8 as pricePerUnitVAT,
+    t.year,
+    'DEMO' as AnalyzaName ,
+    j.created
+
+FROM InDocTables t
+         join InDocJobs j on t.pk = j.tablePK
+         join SmlouvyIds s on s.Id = t.smlouvaID
+         join SmlouvyDodavatele d on s.Id = d.SmlouvaId
+where
+    (t.subject = 'DEMO' or category between 10000 and 10099)
+  --(t.subject = 'it' and category between 10000 and 10099)
+
+  and t.year=2018
+  and j.jobGrouped is not null and j.jobGrouped != '0'
+	and j.PriceVATCalculated is not null
+	and (j.pk not in (select jobpk from Ceny))
+    and Unit = 1 --manday
 
