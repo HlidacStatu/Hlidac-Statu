142f4c523aa74271975b3537a12f6539596ff812
suchoss <suchoss@gmail.com>
2022-04-25 18:09:01 +0200

    Remove unused columns in jobs

---
+++ DBUpgrader/V1/UpgradeDB.1.0.0.110.cs
@@ -0,0 +1,22 @@
+﻿using Devmasters.DatabaseUpgrader;
+
+
+namespace HlidacStatu.DBUpgrades
+{
+    public static partial class DBUpgrader
+    {
+        private partial class UpgradeDB
+        {
+            [DatabaseUpgradeMethod("1.0.0.110")]
+            public static void Init_1_0_0_110(IDatabaseUpgrader du)
+            {
+                string sql = @"
+alter table indoctables drop column subject;
+alter table indocjobs drop column salarymd;
+alter table indocjobs drop column salarymdvat;
+";
+                du.RunDDLCommands(sql);
+            }
+        }
+    }
+}
