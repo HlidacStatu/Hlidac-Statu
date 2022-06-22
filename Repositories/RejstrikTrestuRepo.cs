edfc279f4ccbd40e48de737b4f17e298c5629d6f
Michal <michal@michalblaha.cz>
2022-06-22 16:08:17 +0200

    Update RejstrikTrestuRepo.cs

--- Repositories/RejstrikTrestuRepo.cs
+++ Repositories/RejstrikTrestuRepo.cs
@@ -15,26 +15,21 @@ public static class RejstrikTrestuRepo
     {
         var client = await Manager.GetESClientAsync(_indexName, idxType: _indexType);
 
-        string query = @$"{{
-            ""bool"": {{
-              ""must"": [
-                {{
-                  ""match"": {{
-                    ""ICO"": ""{ico}""
-                  }}
-                }}
-              ]
-            }}
-          }}";
-
         try
         {
             var result = await client.SearchAsync<RejstrikTrestu>(s => s
-                .Query(q => q.Raw(query))
+                .Query(q => q
+                    .Match(m=>m
+                        .Field(f=>f.ICO)
+                        .Query(ico)
+                        )
+                    )
+                .Sort(s=>s.Descending(f=>f.DatumPravniMoci))
             );
 
 
-            return result.Documents.ToList();
+            var res = result.Documents.ToList();
+            return res;
         }
         catch (Exception e)
         {
@@ -48,28 +43,51 @@ public static class RejstrikTrestuRepo
 
 public class RejstrikTrestu
 {
-    public string Id { get; }
+    [Nest.PropertyName("Id")]
+    public string Id { get; set; }
+
+    [Nest.PropertyName("ICO")]
     public string ICO { get; set; }
+    [Nest.PropertyName("NazevFirmy")]
     public string NazevFirmy { get; set; }
+
+    [Nest.PropertyName("Sidlo")]
     public string Sidlo { get; set; }
+
+    [Nest.PropertyName("RUIANCode")]
     public string RUIANCode { get; set; }
+
+    [Nest.PropertyName("DatumPravniMoci")]
     public DateTime DatumPravniMoci { get; set; }
 
+    [Nest.PropertyName("Odsouzeni")]
     public odsouzeni Odsouzeni { get; set; }
 
     public class odsouzeni
     {
+
+        [Nest.PropertyName("PrvniInstance")]
         public Soud PrvniInstance { get; set; }
+
+        [Nest.PropertyName("OdvolaciSoud")]
         public Soud OdvolaciSoud { get; set; }
     }
 
+
+    [Nest.PropertyName("Paragrafy")]
     public Paragraf[] Paragrafy { get; set; }
 
+
+    [Nest.PropertyName("Tresty")]
     public Trest[] Tresty { get; set; }
 
     public class Trest
     {
+
+        [Nest.PropertyName("Druh")]
         public string Druh { get; set; }
+
+        [Nest.PropertyName("DruhText")]
         public string DruhText { get; set; }
 
         public Severity Riziko() => Druh.ToLower() switch
@@ -99,29 +117,55 @@ public class RejstrikTrestu
 
     public class Soud
     {
+
+        [Nest.PropertyName("DruhRozhodnuti")]
         public string DruhRozhodnuti { get; set; }
+
+        [Nest.PropertyName("DatumRozhodnuti")]
         public DateTime DatumRozhodnuti { get; set; }
+
+        [Nest.PropertyName("Jmeno")]
         public string Jmeno { get; set; }
+
+        [Nest.PropertyName("SpisovaZnacka")]
         public string SpisovaZnacka { get; set; }
     }
 
     public class Paragraf
     {
+
+        [Nest.PropertyName("Zakon")]
         public zakon Zakon { get; set; }
 
         public class zakon
         {
+
+            [Nest.PropertyName("Rok")]
             public int Rok { get; set; }
+
+            [Nest.PropertyName("ZakonCislo")]
             public string ZakonCislo { get; set; }
+
+            [Nest.PropertyName("ParagrafCislo")]
             public string ParagrafCislo { get; set; }
+
+            [Nest.PropertyName("OdstavecPismeno")]
             public string OdstavecPismeno { get; set; }
         }
 
+
+        [Nest.PropertyName("ZakonPopis")]
         public string ZakonPopis { get; set; }
+
+        [Nest.PropertyName("Zavineni")]
         public string Zavineni { get; set; }
+
+        [Nest.PropertyName("Doplneni")]
         public string Doplneni { get; set; }
     }
 
 
+
+    [Nest.PropertyName("TextOdsouzeni")]
     public string TextOdsouzeni { get; set; }
 }
