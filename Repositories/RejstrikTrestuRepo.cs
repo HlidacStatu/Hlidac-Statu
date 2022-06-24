e5d4706f08304a8eb0df141cf2f796b1eab31890
suchoss <suchoss@gmail.com>
2022-06-24 10:10:06 +0200

    Rizika

--- Repositories/RejstrikTrestuRepo.cs
+++ Repositories/RejstrikTrestuRepo.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HlidacStatu.Entities.Entities;
 using HlidacStatu.Repositories.ES;
+using Nest;
 
 namespace HlidacStatu.Repositories;
 
@@ -36,133 +38,41 @@ public static class RejstrikTrestuRepo
 
         return new List<RejstrikTrestu>();
     }
-}
-
-public class RejstrikTrestu
-{
-    [Nest.PropertyName("Id")]
-    public string Id { get; set; }
-
-    [Nest.PropertyName("ICO")]
-    public string ICO { get; set; }
-    [Nest.PropertyName("NazevFirmy")]
-    public string NazevFirmy { get; set; }
-
-    [Nest.PropertyName("Sidlo")]
-    public string Sidlo { get; set; }
-
-    [Nest.PropertyName("RUIANCode")]
-    public string RUIANCode { get; set; }
-
-    [Nest.PropertyName("DatumPravniMoci")]
-    public DateTime DatumPravniMoci { get; set; }
-
-    [Nest.PropertyName("Odsouzeni")]
-    public odsouzeni Odsouzeni { get; set; }
-
-    public class odsouzeni
+    
+    public static async Task<RejstrikTrestu> FindTrestByIdAsync(string id)
     {
+        var client = await Manager.GetESClientAsync(_indexName, idxType: _indexType);
 
-        [Nest.PropertyName("PrvniInstance")]
-        public Soud PrvniInstance { get; set; }
-
-        [Nest.PropertyName("OdvolaciSoud")]
-        public Soud OdvolaciSoud { get; set; }
-    }
-
-
-    [Nest.PropertyName("Paragrafy")]
-    public Paragraf[] Paragrafy { get; set; }
-
-
-    [Nest.PropertyName("Tresty")]
-    public Trest[] Tresty { get; set; }
-
-    public class Trest
-    {
-
-        [Nest.PropertyName("Druh")]
-        public string Druh { get; set; }
-
-        [Nest.PropertyName("DruhText")]
-        public string DruhText { get; set; }
-
-        public Severity Riziko => Druh.ToLower() switch
+        try
         {
-            "zpo" => Severity.Fatal,
-            "zc" or "zpvzuvs" or "zpvz" or "zpds" => Severity.Critical,
-            "pt" or "zv" or "pv" or "pvjmh" or "pm" or "pnh" => Severity.Normal,
-            _ => Severity.Others
-        };
+            var response = await client.GetAsync<RejstrikTrestu>(id);
 
-        [Devmasters.Enums.ShowNiceDisplayName]
-        public enum Severity
-        {
-            [Devmasters.Enums.NiceDisplayName("Fatální")]
-            Fatal = 3,
-
-            [Devmasters.Enums.NiceDisplayName("Vysoké riziko")]
-            Critical = 2,
-
-            [Devmasters.Enums.NiceDisplayName("Nízké riziko")]
-            Normal = 1,
-
-            [Devmasters.Enums.NiceDisplayName("Ostatní")]
-            Others = 0,
+            return response.IsValid
+                ? response.Source
+                : null;
         }
-    }
-
-    public class Soud
-    {
-
-        [Nest.PropertyName("DruhRozhodnuti")]
-        public string DruhRozhodnuti { get; set; }
-
-        [Nest.PropertyName("DatumRozhodnuti")]
-        public DateTime DatumRozhodnuti { get; set; }
-
-        [Nest.PropertyName("Jmeno")]
-        public string Jmeno { get; set; }
-
-        [Nest.PropertyName("SpisovaZnacka")]
-        public string SpisovaZnacka { get; set; }
-    }
-
-    public class Paragraf
-    {
-
-        [Nest.PropertyName("Zakon")]
-        public zakon Zakon { get; set; }
-
-        public class zakon
+        catch (Exception e)
         {
-
-            [Nest.PropertyName("Rok")]
-            public int Rok { get; set; }
-
-            [Nest.PropertyName("ZakonCislo")]
-            public string ZakonCislo { get; set; }   // somewhere is int
-
-            [Nest.PropertyName("ParagrafCislo")]
-            public string ParagrafCislo { get; set; } // somewhere is int
-
-            [Nest.PropertyName("OdstavecPismeno")]
-            public string OdstavecPismeno { get; set; }
+            // ignored
         }
 
-
-        [Nest.PropertyName("ZakonPopis")]
-        public string ZakonPopis { get; set; }
-
-        [Nest.PropertyName("Zavineni")]
-        public string Zavineni { get; set; }
-
-        [Nest.PropertyName("Doplneni")]
-        public string Doplneni { get; set; }
+        return null;
     }
-
-
-
-    [Nest.PropertyName("TextOdsouzeni")]
-    public string TextOdsouzeni { get; set; }
+    
+    // public static async Task SaveAsync(RejstrikTrestu rejstrikTrestu)
+    // {
+    //     if (rejstrikTrestu == null) throw new ArgumentNullException(nameof(rejstrikTrestu));
+    //     
+    //     
+    //     var client = await Manager.GetESClientAsync(_indexName, idxType: _indexType);
+    //
+    //     var res = await client.IndexAsync(rejstrikTrestu, o => o.Id(rejstrikTrestu.Id));
+    //     if (!res.IsValid)
+    //     {
+    //         throw new ApplicationException(res.ServerError?.ToString());
+    //     }
+    // }
+    
+    
+    
 }
