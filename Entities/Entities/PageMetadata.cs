1a9057a75e882ed2d028e1a83762c57399dd6817
Michal <michal@michalblaha.cz>
2022-07-08 12:40:16 +0200

    Create PageMetadata.cs

---
+++ Entities/Entities/PageMetadata.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HlidacStatu.Entities.Entities
+{
+    public class PageMetadata
+    {
+        [Nest.Keyword()]
+        public string SmlouvaId { get; set; }
+        [Nest.Keyword()]
+        public string PrilohaId { get; set; }
+
+        [Nest.Number]
+        public int? PageNum { get; set; }
+
+
+
+        public class Conceal
+        {
+            public int ImageWidth { get; set; }
+            public int ImageHeight { get; set; }
+
+            public long TextArea { get; set; }
+            public long BlackedArea { get; set; }
+
+            [Nest.Date]
+            public DateTime Created { get; set; }
+
+            [Nest.Keyword()]
+            public string AnalyzerVersion { get; set; }
+
+            public System.Drawing.RectangleF TextAreaBoundaries { get; set; }
+
+            public System.Drawing.RectangleF BlackedAreaBoundaries { get; set; }
+
+            public decimal BlackedAreaRatio()
+            {
+                return (decimal)BlackedArea / (decimal)(BlackedArea + TextArea);
+            }
+        }
+    }
+}
