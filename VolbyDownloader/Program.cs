4fac7f4d8fbb74d7253155691f7e3440f215a480
Suchoss <suchoss@gmail.com>
2022-08-16 15:20:15 +0200

    small cleanup

--- VolbyDownloader/Program.cs
+++ VolbyDownloader/Program.cs
@@ -5,11 +5,14 @@ using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Serialization;
 using CsvHelper;
+using Devmasters.Log;
 using HlidacStatu.Entities;
 using Microsoft.Extensions.Configuration;
 using Polly;
 using Polly.Extensions.Http;
 using Polly.Retry;
+using Serilog;
+using Serilog.Events;
 using MemoryStream = System.IO.MemoryStream;
 using StreamReader = System.IO.StreamReader;
 
@@ -22,7 +25,20 @@ class Program
 
     public static async Task Main(string[] args)
     {
-        //init
+        #region init
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Is(LogEventLevel.Debug)
+            .WriteTo.Console()
+            //.AddLogStash(new Uri(Global.LogStashUrl))
+            .Enrich.WithProperty("hostname", Environment.MachineName)
+            .Enrich.WithProperty("codeversion",
+                System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version?.ToString())
+            .Enrich.WithProperty("application_name", "VolbyDownloader")
+            .CreateLogger();
+
+        var logger = Log.ForContext<Program>();
+
         IConfiguration configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", false)
@@ -38,22 +54,27 @@ class Program
 
         using var httpClient = new HttpClient();
 
+        #endregion
+
         //todo: jak to bude s mazáním dat před downloadem??        
 
-        //todo: change to tasks and run them at the same time
-        //await DownloadOvmsAsync(httpClient, ovmUrl);
-        await DownloadCuzkAsync(httpClient, cuzkUrl);
+        // nechci je spouštět najednou, protože habruje připojení k sql :(
+        await DownloadOvmsAsync(httpClient, ovmUrl, logger);
+        await DownloadCuzkAsync(httpClient, cuzkUrl, logger);
+
+        Console.WriteLine("Done");
     }
 
-    public static async Task DownloadCuzkAsync(HttpClient httpClient, string cuzkUrl)
+    public static async Task DownloadCuzkAsync(HttpClient httpClient, string cuzkUrl, ILogger logger)
     {
         using var responseMessage = await _retryPolicy.ExecuteAsync(() => httpClient.GetAsync(cuzkUrl));
 
         if (!responseMessage.IsSuccessStatusCode)
         {
-            // _logger.Error("Error during {methodName}. Server responded with [{statusCode}] status code. Reason phrase [{reasonPhrase}].",
-            //     nameof(GetNewTaskAsync), responseMessage.StatusCode, responseMessage.ReasonPhrase);
-            return;
+            logger.Error(
+                "Error during {methodName}. Server responded with [{statusCode}] status code. Reason phrase [{reasonPhrase}].",
+                nameof(DownloadCuzkAsync), responseMessage.StatusCode, responseMessage.ReasonPhrase);
+            throw new Exception($"Can't download data from {cuzkUrl}");
         }
 
         var page = await responseMessage.Content.ReadAsStringAsync();
@@ -68,14 +89,16 @@ class Program
         var encoding = Encoding.GetEncoding(1250);
         string delimiter = ";";
 
-        //var semaphoreSlim = new SemaphoreSlim(3, 3);
-        //List<Task> downloadTasks = new();
+
+        // následující zakomentovaná sekce nefungovala, kvůli padajícímu poolu (timeout) na SQL serveru. 
+        // var semaphoreSlim = new SemaphoreSlim(1, 1);
+        // List<Task> downloadTasks = new();
         foreach (Match match in matches)
         {
             var url = match.Groups[matchGroupName].Value;
 
-            //await semaphoreSlim.WaitAsync();
-            await DownloadSestavaAsync(httpClient, url, culture, encoding, delimiter);
+            // await semaphoreSlim.WaitAsync();
+            await DownloadSestavaAsync(httpClient, url, culture, encoding, delimiter, logger);
             // try
             // {
             //     downloadTasks.Add(DownloadSestavaAsync(httpClient, url, culture, encoding, delimiter));
@@ -86,22 +109,23 @@ class Program
             // }
         }
 
-        //await Task.WhenAll(downloadTasks.ToArray());
+        // await Task.WhenAll(downloadTasks.ToArray());
     }
 
     private static async Task DownloadSestavaAsync(HttpClient httpClient, string url, CultureInfo culture,
-        Encoding encoding, string delimiter)
+        Encoding encoding, string delimiter, ILogger logger)
     {
-        using var httpResponseMessage = await _retryPolicy.ExecuteAsync(() => httpClient.GetAsync(url));
+        using var responseMessage = await _retryPolicy.ExecuteAsync(() => httpClient.GetAsync(url));
 
-        if (!httpResponseMessage.IsSuccessStatusCode)
+        if (!responseMessage.IsSuccessStatusCode)
         {
-            // _logger.Error("Error during {methodName}. Server responded with [{statusCode}] status code. Reason phrase [{reasonPhrase}].",
-            //     nameof(GetNewTaskAsync), responseMessage.StatusCode, responseMessage.ReasonPhrase);
-            return;
+            logger.Error(
+                "Error during {methodName}. Server responded with [{statusCode}] status code. Reason phrase [{reasonPhrase}].",
+                nameof(DownloadSestavaAsync), responseMessage.StatusCode, responseMessage.ReasonPhrase);
+            throw new Exception($"Can't download data from {url}");
         }
 
-        var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
+        var stream = await responseMessage.Content.ReadAsStreamAsync();
 
         using var zipArchive = new ZipArchive(stream, ZipArchiveMode.Read);
         var csvEntries = zipArchive.Entries.Where(e =>
@@ -121,33 +145,39 @@ class Program
             {
                 var records = csv.GetRecords<AdresniMisto>();
 
-                using (var db = new DbEntities())
+                var chunks = records.Chunk(100);
+                foreach (var chunk in chunks)
                 {
-                    db.AdresniMisto.AddRange(records);
+                    using (var db = new DbEntities())
+                    {
+                        db.AdresniMisto.AddRange(chunk);
 
-                    try
-                    {
-                        await db.SaveChangesAsync();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                        throw;
+                        try
+                        {
+                            await db.SaveChangesAsync();
+                        }
+                        catch (Exception e)
+                        {
+                            logger.Error(e, "problém při zápisu dat z {url}, entryname={entryName} ", url,
+                                csvEntry.FullName);
+                            throw;
+                        }
                     }
                 }
             }
         }
     }
 
-    public static async Task DownloadOvmsAsync(HttpClient httpClient, string ovmUrl)
+    public static async Task DownloadOvmsAsync(HttpClient httpClient, string ovmUrl, ILogger logger)
     {
         using var responseMessage = await _retryPolicy.ExecuteAsync(() => httpClient.GetAsync(ovmUrl));
 
         if (!responseMessage.IsSuccessStatusCode)
         {
-            // _logger.Error("Error during {methodName}. Server responded with [{statusCode}] status code. Reason phrase [{reasonPhrase}].",
-            //     nameof(GetNewTaskAsync), responseMessage.StatusCode, responseMessage.ReasonPhrase);
-            return;
+            logger.Error(
+                "Error during {methodName}. Server responded with [{statusCode}] status code. Reason phrase [{reasonPhrase}].",
+                nameof(DownloadOvmsAsync), responseMessage.StatusCode, responseMessage.ReasonPhrase);
+            throw new Exception($"Can't download data from {ovmUrl}");
         }
 
         var stream = await responseMessage.Content.ReadAsStreamAsync();
@@ -167,7 +197,7 @@ class Program
             Dictionary<string, PravniFormaOvm> pfOvm = new();
             Dictionary<int, AdresaOvm> adresyOvm = new();
 
-            var chunks = test.Subjekt.Chunk(100); // we need to make smaller chunks, so it wont timeout
+            var chunks = test.Subjekt.Chunk(100);
             foreach (var chunk in chunks)
             {
                 using (var db = new DbEntities())
