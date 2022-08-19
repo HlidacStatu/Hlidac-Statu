119b319bf740d3fba9397b72b12f0977cb52a47a
Suchoss <suchoss@gmail.com>
2022-08-19 21:08:44 +0200

    Adding address parser and optimizing autocomplete component

--- Entities/Views/AdresyKVolbam.cs
+++ Entities/Views/AdresyKVolbam.cs
@@ -9,21 +9,13 @@ public class AdresyKVolbam : IEquatable<AdresyKVolbam>
     public int Id { get; set; }
 
     [FullTextSearch.Search]
-    public string OneLiner { get; set; }
-
-    public string Ulice { get; set; }
-    public int? CisloDomovni { get; set; }
-    public string Psc { get; set; }
-    public string Obec { get; set; }
+    public string Adresa { get; set; }
 
     public int TypOvm { get; set; }
-    
-    public string NazevUradu { get; set; }
-    public string ObecUradu { get; set; }
-    public string PscUradu { get; set; }
-    public string UliceUradu { get; set; }
-    public int? CisloDomovniUradu { get; set; }
     public string DatovkaUradu { get; set; }
+    public string NazevUradu { get; set; }
+    public string AdresaUradu { get; set; }
+    
 
     public bool Equals(AdresyKVolbam other)
     {
