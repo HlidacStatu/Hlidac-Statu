1ba1a33595214d9089f4cf267bf2042495f5b356
Suchoss <suchoss@gmail.com>
2022-09-20 13:47:14 +0200

    Removing FulltextSearch

--- AutocompleteApi/Controllers/AutocompleteController.cs
+++ AutocompleteApi/Controllers/AutocompleteController.cs
@@ -25,6 +25,33 @@ namespace HlidacStatu.AutocompleteApi.Controllers
             return searchResult.Select(r => r.Original);
         }
 
+        public IEnumerable<> Kindex(string q)
+        {
+            //Web/controllers/KindexController
+        }
+        
+        public IEnumerable<> Companies(string q)
+        {
+            //Web/controllers/apiv1controller
+        }
+
+        public IEnumerable<> UptimeServer(string q)
+        {
+            //HlidacStatu.Repositories.uptimeserverrepo
+        }
+
+
+            // private static CachedIndex<Autocomplete> CompanyAutocomplete = new(
+            //     Path.Combine(Init.WebAppDataPath, "autocomplete", "np_firmy"),
+            //     TimeSpan.FromDays(30),
+            //     () => StaticData.Autocomplete_Firmy_Cache.Get(),
+            //     new IndexingOptions<Autocomplete>()
+            //     {
+            //         TextSelector = ts => $"{ts.Text}"
+            //     });
+        
+        
+
         [HttpGet]
         public IEnumerable<Autocomplete> TestAutocomplete(string q)
         {
---
+++ ObjectMinifier/Compressor.cs
@@ -0,0 +1,55 @@
+﻿using System.IO.Compression;
+using System.Text.Json;
+
+namespace ObjectMinifier;
+
+public static class Compressor
+{
+    public static void CompressToFile<T>(T data, string path)
+    {
+        var utf8Bytes = JsonSerializer.SerializeToUtf8Bytes(data);
+        var compressedData = Compress(utf8Bytes);
+        File.WriteAllBytes(path, compressedData);
+    }
+    
+    public static T ReadFromFile<T>(string path)
+    {
+        var bytes = File.ReadAllBytes(path);
+        var decompressed = Decompress(bytes);
+        var deserialized = JsonSerializer.Deserialize<T>(decompressed);
+        return deserialized;
+    }
+    
+    public static byte[] CompressToByteArr<T>(T data)
+    {
+        var utf8Bytes = JsonSerializer.SerializeToUtf8Bytes(data);
+        return Compress(utf8Bytes);
+    }
+    
+    public static T ReadFromByteArr<T>(byte[] bytes)
+    {
+        var decompressed = Decompress(bytes);
+        var deserialized = JsonSerializer.Deserialize<T>(decompressed);
+        return deserialized;
+    }
+    
+    private static byte[] Compress(byte[] bytes)
+    {
+        using var outputStream = new MemoryStream();
+        using var compressStream = new BrotliStream(outputStream, CompressionLevel.Fastest);
+        compressStream.Write(bytes, 0, bytes.Length);
+        compressStream.Flush();
+
+        return outputStream.ToArray();
+    }
+
+    private static byte[] Decompress(byte[] bytes)
+    {
+        using var inputStream = new MemoryStream(bytes);
+        using var outputStream = new MemoryStream();
+        using var decompressStream = new BrotliStream(inputStream, CompressionMode.Decompress);
+
+        decompressStream.CopyTo(outputStream);
+        return outputStream.ToArray();
+    }
+}
