3aa00453e2c79b6a4210f37202261f51276fc455
Suchoss <suchoss@gmail.com>
2022-10-07 08:59:10 +0200

    update unique constraint

---
+++ DBUpgrader/V1/UpgradeDB.1.0.0.115.cs
@@ -0,0 +1,18 @@
+﻿using Devmasters.DatabaseUpgrader;
+
+namespace HlidacStatu.DBUpgrades
+{
+    public static partial class DBUpgrader
+    {
+        private partial class UpgradeDB
+        {
+            [DatabaseUpgradeMethod("1.0.0.115")]
+            public static void Init_1_0_0_115(IDatabaseUpgrader du)
+            {
+                string sql = @"
+ALTER TABLE dbo.ConfigurationValues ADD CONSTRAINT uniqueKeys UNIQUE (KeyName, Environment, Tag);";
+                du.RunDDLCommands(sql);
+            }
+        }
+    }
+}
