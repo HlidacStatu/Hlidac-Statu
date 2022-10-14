c200290a9d39142d05d3d60aac126a6d2ee0a419
Suchoss <suchoss@gmail.com>
2022-10-14 13:01:59 +0200

    Moving project to Downloader

--- VolbyDownloader/OvmXml.cs
+++
@@ -1,9 +0,0 @@
-using System.Xml.Serialization;
-using HlidacStatu.Entities;
-
-[XmlRoot(ElementName = "SeznamOvmIndex")]
-public class SeznamOvmIndex
-{
-    [XmlElement(ElementName = "Subjekt")]
-    public List<OrganVerejneMoci> Subjekt { get; set; }
-}
