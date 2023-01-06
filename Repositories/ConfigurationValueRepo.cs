ce04459414e8155a376687d8db8bab60ae2e5bc8
Suchoss <suchoss@gmail.com>
2023-01-06 19:34:31 +0100

    Finish configurationValueRepo

--- Repositories/ConfigurationValueRepo.cs
+++ Repositories/ConfigurationValueRepo.cs
@@ -10,7 +10,7 @@ namespace HlidacStatu.Repositories;
 
 public static class ConfigurationValueRepo
 {
-    public static async Task<List<ConfigurationValue>> GetConfigurationValues()
+    public static async Task<List<ConfigurationValue>> GetConfigurationValuesAsync()
     {
         await using var dbContext = new DbEntities();
 
@@ -18,25 +18,40 @@ public static class ConfigurationValueRepo
         return await confValues.ToListAsync();
     }
 
-    public static async Task<ConfigurationValue> Find(ConfigurationValue configurationValue)
-    {
-        if (configurationValue is null)
-            return default;
-
-        await using var dbContext = new DbEntities();
-        return await dbContext.ConfigurationValues.AsNoTracking()
-            .Where(cv => cv.KeyName == configurationValue.KeyName)
-            .Where(cv => cv.KeyValue == configurationValue.KeyValue)
-            .Where(cv => cv.Environment == configurationValue.Environment)
-            .Where(cv => cv.Tag == configurationValue.Tag)
-            .SingleOrDefaultAsync();
-    }
-
-    public static async Task UpsertKey(ConfigurationValue configurationValue)
+    public static async Task UpsertAsync(ConfigurationValue configurationValue)
     {
         await using var dbContext = new DbEntities();
+
+        if (configurationValue.Id == 0)
+        {
+            //insert
+            dbContext.ConfigurationValues.Add(configurationValue);
+        }
+        else
+        {
+            var existingConfig =
+                await dbContext.ConfigurationValues.FirstOrDefaultAsync(q => q.Id == configurationValue.Id);
+
+            existingConfig.Tag = configurationValue.Tag;
+            existingConfig.Environment = configurationValue.Environment;
+            existingConfig.KeyValue = configurationValue.KeyValue;
+            existingConfig.KeyName = configurationValue.KeyName;
+        }
+
+        await dbContext.SaveChangesAsync();
+        
         throw new NotImplementedException("musime to dokoncit - podzemkastav");
 
     }
 
+    public static async Task DeleteAsync(ConfigurationValue configurationValue)
+    {
+        if (configurationValue is null)
+            return;
+        
+        await using var dbContext = new DbEntities();
+        dbContext.ConfigurationValues.Remove(configurationValue);
+        await dbContext.SaveChangesAsync();
+
+    }
 }
