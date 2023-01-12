46ba80e9bc5d10e2fa98c188fead500a967f7177
Michal Blaha <michal@michalblaha.cz>
2023-01-12 20:51:50 +0100

    Update Tool.cs

--- Repositories/ProfilZadavatelu/Tool.cs
+++ Repositories/ProfilZadavatelu/Tool.cs
@@ -30,7 +30,7 @@ namespace HlidacStatu.Repositories.ProfilZadavatelu
                         return new ActionOutputData();
                     }, null,
                     outputWriter ?? Manager.DefaultOutputWriter, progressWriter ?? Manager.DefaultProgressWriter,
-                    false, elasticClient: await ES.Manager.GetESClient_VZAsync(), prefix: "profil zadavatelu ", monitor: new MonitoredTaskRepo.ForBatch());
+                    false, elasticClient: await ES.Manager.GetESClient_VZAsync(), prefix: "profil zadavatelu ");
 
                 Console.WriteLine("Let's go mining");
 
