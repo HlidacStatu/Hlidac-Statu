0e7c43952f1d421c301230116a4600e052bb5349
Michal Blaha <michal@michalblaha.cz>
2023-02-20 21:01:04 +0100

    privacy

---
+++ HlidacStatu.JobsWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Net.Mail;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HlidacStatu.Ceny.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<KontaktModel> _logger;
+
+        public PrivacyModel(ILogger<KontaktModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+ 
+    }
+}
