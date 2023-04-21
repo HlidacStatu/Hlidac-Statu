57492905a2851a3d6b7925c12395f1729c46220e
Suchoss <suchoss@gmail.com>
2023-04-21 19:44:57 +0200

    Fix pdf links for some browsers

---
+++ HlidacStatu.JobsWeb/Helpers/Helpers.cs
@@ -0,0 +1,19 @@
+using Devmasters;
+
+namespace HlidacStatu.Ceny.Helpers;
+
+public static class Helpers
+{
+    public static string MakeIdFromName(this string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return string.Empty;
+        
+        return name.RemoveDiacritics()
+            .Replace(" ", "_")
+            .Replace(".", "_")
+            .Replace("/", "_")
+            .Replace("\\", "_")
+            .ToLower();
+    }
+}
