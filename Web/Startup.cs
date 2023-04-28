f4550e3f86a516a927ff9284ebff2376e1e6f162
Michal Blaha <michal@michalblaha.cz>
2023-04-28 17:12:04 +0200

    log fixes

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -208,14 +208,14 @@ namespace HlidacStatu.Web
             Devmasters.Log.Logger webExceptionLogger = Devmasters.Log.Logger.CreateLogger("HlidacStatu.Web.Exceptions",
                 Devmasters.Log.Logger.DefaultConfiguration()
                     .Enrich.WithProperty("codeversion", System.Reflection.Assembly.GetEntryAssembly().GetName().Version.ToString())
-                    //.AddLogStash(new Uri("http://10.10.150.203:5000"))
+                    .AddLogStash(new Uri("http://10.10.150.203:5000"))
                     .AddFileLoggerFilePerLevel(logpath2, "slog.txt",
                         outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {SourceContext} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
                         rollingInterval: Serilog.RollingInterval.Day,
                         fileSizeLimitBytes: null,
                         retainedFileCountLimit: 9,
                         shared: true
-                    ),false);
+                    ));
             app.UseOnHTTPErrorMiddleware(webExceptionLogger);
 
             app.UseStaticFiles();
@@ -268,8 +268,8 @@ namespace HlidacStatu.Web
             app.UseApiAuthenticationMiddleware();
             
             app.UseAuthorization();
-            
 
+            Util.Consts.Logger.Info("Starting web application {version}", System.Reflection.Assembly.GetEntryAssembly().GetName().Version.ToString());
 
             _ = app.UseEndpoints(endpoints =>
             {
