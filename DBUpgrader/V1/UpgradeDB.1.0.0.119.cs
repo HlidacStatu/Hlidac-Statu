8d46b53659eeae96d20c71bf2ac496022c2f7b66
Suchoss <suchoss@gmail.com>
2023-05-17 07:36:09 +0200

    Merk closing date

---
+++ DBUpgrader/V1/UpgradeDB.1.0.0.119.cs
@@ -0,0 +1,16 @@
+﻿using Devmasters.DatabaseUpgrader;
+
+namespace HlidacStatu.DBUpgrades
+{
+    public static partial class DBUpgrader
+    {
+        private partial class UpgradeDB
+        {
+            [DatabaseUpgradeMethod("1.0.0.119")]
+            public static void Init_1_0_0_119(IDatabaseUpgrader du)
+            {
+                du.AddColumnToTable("DatumZaniku", "datetime", "Firma", true);
+            }
+        }
+    }
+}
