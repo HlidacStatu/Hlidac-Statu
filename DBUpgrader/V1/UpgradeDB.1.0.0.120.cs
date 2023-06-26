161163580ebaa82a81099ab3dd0a9b6972b8ea65
Suchoss <suchoss@gmail.com>
2023-06-26 17:16:42 +0200

    Create a new table platy uredniku

---
+++ DBUpgrader/V1/UpgradeDB.1.0.0.120.cs
@@ -0,0 +1,34 @@
+﻿using Devmasters.DatabaseUpgrader;
+
+namespace HlidacStatu.DBUpgrades
+{
+    public static partial class DBUpgrader
+    {
+        private partial class UpgradeDB
+        {
+            [DatabaseUpgradeMethod("1.0.0.120")]
+            public static void Init_1_0_0_120(IDatabaseUpgrader du)
+            {
+                string sql = @"
+CREATE TABLE [dbo].[PlatyUredniku](
+	[pk] [bigint] IDENTITY(1,1) PRIMARY KEY,
+	[Ico] [nvarchar](30),
+	[DruhInstituce] [nvarchar](500),
+    [NazevInstituce] [nvarchar](500),
+    [Pozice] [nvarchar](500),
+    [NazevPlatu] [nvarchar](500),
+    [rok] [int], 
+    [plat] [decimal],
+    [odmeny] [decimal],
+    [pocetmes] [int],
+    [bonus] [decimal],
+    [nefbonus] [nvarchar](max)
+    );
+
+CREATE NONCLUSTERED INDEX IX_platyuredniku ON dbo.PlatyUredniku (Ico); 
+";
+                du.RunDDLCommands(sql);
+            }
+        }
+    }
+}
