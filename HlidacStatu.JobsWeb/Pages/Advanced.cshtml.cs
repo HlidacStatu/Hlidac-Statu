5898c3b9ab2daa49224142b4f3cf2a1026d42e56
Michal Blaha <michal@michalblaha.cz>
2023-07-15 14:40:54 +0200

    advanced stats

---
+++ HlidacStatu.JobsWeb/Pages/Advanced.cshtml.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using HlidacStatu.Ceny.Models;
+using HlidacStatu.Ceny.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HlidacStatu.Ceny.Pages
+{
+    [Authorize(Roles = "Admin")]
+    public class AdvancedModel : PageModel
+    {
+        public YearlyStatisticsGroup.Key? Key { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var obor_rok =JobService.TryFindKey(HttpContext);
+
+            var ad = await HttpContext.HasAccess();
+            if (ad.Access == false)
+                return Redirect("/");
+            if (ad.AnalyzeLevel < Entities.CenyCustomer.AccessDetail.AccessDetailLevel.PRO)
+                return Redirect($"/souhrn?{obor_rok.Value.UrlDecodedParams}");
+
+            Key = HttpContext.TryFindKey();
+            return Page();
+        }
+
+    }
+}
--- Repositories/CenyCustomerRepo.cs
+++ Repositories/CenyCustomerRepo.cs
@@ -1,6 +1,7 @@
 ﻿using HlidacStatu.Entities;
 
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace HlidacStatu.Repositories
@@ -18,14 +19,21 @@ namespace HlidacStatu.Repositories
 
             using (var db = new DbEntities())
             {
-                var ret = await db.CenyCustomer.AsQueryable()
-                    .FirstOrDefaultAsync(m => m.Username == username && m.Analyza == analyza && m.Rok == rok && m.Paid.HasValue);
-                if (ret == null)
+                var retAll = await db.CenyCustomer.AsQueryable()
+                    .Where(m => m.Username == username && m.Analyza == analyza && m.Rok == rok && m.Paid.HasValue)
+                    .ToListAsync();
+
+                if (retAll == null || retAll.Count()==0)
                     return CenyCustomer.AccessDetail.NoAccess();
                 else
                 {
-                    //TODO zmenit podle castky
-                    return new CenyCustomer.AccessDetail(CenyCustomer.AccessDetail.AccessDetailLevel.PRO );
+                    //TODO zmenit podle levelu
+                    if (retAll.Max(m=>m.Level) == 1)
+                        return new CenyCustomer.AccessDetail(CenyCustomer.AccessDetail.AccessDetailLevel.BASIC);
+                    else if (retAll.Max(m => m.Level) == 2)
+                        return new CenyCustomer.AccessDetail(CenyCustomer.AccessDetail.AccessDetailLevel.PRO );
+                    else
+                        return CenyCustomer.AccessDetail.NoAccess();
 
                 }
             }
