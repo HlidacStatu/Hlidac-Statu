0e91892b22082aa528e9044fcbae0b57045d8873
Suchoss <suchoss@gmail.com>
2024-02-13 09:28:52 +0100

    Basic graph fixes

--- PlatyUredniku/Models/AreaRangePlot.cs
+++ PlatyUredniku/Models/AreaRangePlot.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PlatyUredniku.Models;
@@ -6,30 +7,39 @@ public class AreaRangePlot
 {
     public string? Title { get; set; }
     public string? Subtitle { get; set; }
-    public int FirstYear { get; set; }
-    public double[][] MinMaxes { get; set; }
-    public double[] Medians { get; set; }
+    public int FirstYear => Values.Keys.Min();
     
-    public double[] CeoSalaries { get; set; }
+    public Dictionary<int, PlotData?> Values { get; set; }
+    
 
+    
     public string DrawMinMaxes()
     {
-        var mnms = MinMaxes.Select(m => $"[{m[0]:F0},{m[1]:F0}]");
+        var data = Values.Select(kvp => $"[Date.UTC({kvp.Key}, 0, 1), {kvp.Value?.Min?.ToString("F0")?? "null"},{kvp.Value?.Max?.ToString("F0")?? "null"}]");
 
-        return $"[{string.Join(",", mnms)}]";
+        return $"[{string.Join(",", data)}]";
     }
     
     public string DrawMedians()
     {
-        var meds = Medians.Select(m => $"[{m:F0}]");
+        var data = Values.Select(kvp => $"[Date.UTC({kvp.Key}, 0, 1), {kvp.Value?.Median?.ToString("F0")?? "null"}]");
 
-        return $"[{string.Join(",", meds)}]";
+        return $"[{string.Join(",", data)}]";
     }
     
     public string DrawCeoSalaries()
     {
-        var salaries = CeoSalaries.Select(m => $"[{m:F0}]");
+        var data = Values.Select(kvp => $"[Date.UTC({kvp.Key}, 0, 1), {kvp.Value?.Ceo?.ToString("F0")?? "null"}]");
 
-        return $"[{string.Join(",", salaries)}]";
+        return $"[{string.Join(",", data)}]";
+    }
+
+    public class PlotData
+    {
+        public double? Min { get; set; }
+        public double? Max { get; set; }
+        public double? Median { get; set; }
+        public double? Ceo { get; set; }
+        
     }
 }
