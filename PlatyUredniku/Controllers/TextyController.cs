f7308afdeda22688c04edbdd5d1ab6ded0c696e6
Suchoss <suchoss@gmail.com>
2024-02-13 13:21:57 +0100

    Patička, hlavička, texty

---
+++ PlatyUredniku/Controllers/TextyController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PlatyUredniku.Controllers;
+
+public class TextyController : Controller
+{
+    public async Task<IActionResult> OProjektu()
+    {
+        return View();
+    }
+    
+    public async Task<IActionResult> PlatyStatnichZamestnancu()
+    {
+        return View();
+    }
+    
+    public async Task<IActionResult> Nejvyssi()
+    {
+        return View();
+    }
+    
+    public async Task<IActionResult> Aktuality()
+    {
+        return View();
+    }
+
+}
