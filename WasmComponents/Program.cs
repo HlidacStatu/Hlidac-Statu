77e3d0527c78a0f9a6a3cb0ecd49f0be86f0d842
Suchoss <suchoss@gmail.com>
2024-03-08 10:19:34 +0100

    Trying to fix trimming

--- WasmComponents/Program.cs
+++ WasmComponents/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WasmComponents.Components.Autocomplete;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(_ => new HttpClient());
+
+var autocompleteWrapHack = typeof(AutocompleteWrap);
 
 await builder.Build().RunAsync();
