d6713b17e4e53f6eb097ed655f7df77a8ba72c1b
Suchoss <suchoss@gmail.com>
2024-04-16 09:33:38 +0200

    Fix bookmarks perf

--- Repositories/BookmarkRepo.cs
+++ Repositories/BookmarkRepo.cs
@@ -1,16 +1,24 @@
 using Devmasters;
-
 using HlidacStatu.Entities;
-
 using Microsoft.EntityFrameworkCore;
-
 using System;
 using System.Linq;
+using ZiggyCreatures.Caching.Fusion;
 
 namespace HlidacStatu.Repositories
 {
     public static class BookmarkRepo
     {
+        private static FusionCache _cache = new FusionCache(new FusionCacheOptions()
+        {
+            DefaultEntryOptions = new FusionCacheEntryOptions
+            {
+                Duration = TimeSpan.FromMinutes(1)
+            },
+            CacheName = "BookmarkRepoCache",
+        });
+        
+
         public static bool IsItemBookmarked(IBookmarkable item, string userId)
         {
             return IsItemBookmarked(Bookmark.ItemTypes.SingleItem, Bookmark.GetBookmarkId(item), userId);
@@ -21,10 +29,16 @@ namespace HlidacStatu.Repositories
             if (string.IsNullOrEmpty(userId))
                 return false;
 
-            using (DbEntities db = new DbEntities())
+            var bookmarks = _cache.GetOrSet<Bookmark[]>($"bookmark:{userId}", _=>
             {
-                return db.Bookmarks.Any(m => m.ItemType == (int)type && m.ItemId == itemId && m.UserId == userId);
-            }
+                using DbEntities db = new DbEntities();
+                return db.Bookmarks.Where(b => b.UserId == userId).ToArray();
+                    
+                    
+            });
+            
+            return bookmarks.Any(m => m.ItemType == (int)type && m.ItemId == itemId);
+
         }
 
         public static void SetBookmark(string name, string url, Bookmark.ItemTypes type, string itemId, string userId)
@@ -43,39 +57,38 @@ namespace HlidacStatu.Repositories
                 UserId = userId
             };
             Save(b);
+            _cache.Remove($"bookmark:{userId}");
         }
 
         public static void DeleteBookmark(Bookmark.ItemTypes type, string itemId, string userId)
         {
-            using (DbEntities db = new DbEntities())
+            using DbEntities db = new DbEntities();
+            var b = db.Bookmarks.FirstOrDefault(m =>
+                m.ItemType == (int)type && m.ItemId == itemId && m.UserId == userId);
+            if (b != null)
             {
-                var b = db.Bookmarks.FirstOrDefault(m =>
-                    m.ItemType == (int)type && m.ItemId == itemId && m.UserId == userId);
-                if (b != null)
-                {
-                    db.Bookmarks.Remove(b);
-                    db.SaveChanges();
-                }
-            }
-        }
-
-        public static void Save(Bookmark bookmark)
-        {
-            using (DbEntities db = new DbEntities())
-            {
-                if (bookmark.Id == default(Guid))
-                {
-                    bookmark.Id = Guid.NewGuid();
-                    db.Bookmarks.Add(bookmark);
-                }
-                else
-                {
-                    db.Bookmarks.Attach(bookmark);
-                    db.Entry(bookmark).State = EntityState.Modified;
-                }
-
+                db.Bookmarks.Remove(b);
                 db.SaveChanges();
             }
+
+            _cache.Remove($"bookmark:{userId}");
+        }
+
+        private static void Save(Bookmark bookmark)
+        {
+            using DbEntities db = new DbEntities();
+            if (bookmark.Id == default(Guid))
+            {
+                bookmark.Id = Guid.NewGuid();
+                db.Bookmarks.Add(bookmark);
+            }
+            else
+            {
+                db.Bookmarks.Attach(bookmark);
+                db.Entry(bookmark).State = EntityState.Modified;
+            }
+
+            db.SaveChanges();
         }
     }
 }
