27446614f04a1d01cabcfa44b79415f46aae1bae
Suchoss <suchoss@gmail.com>
2024-04-29 07:29:45 +0200

    Change elastic back to connection pool

--- Connectors/Manager.cs
+++ Connectors/Manager.cs
@@ -330,18 +330,18 @@ namespace HlidacStatu.Connectors
 
             string esUrl = Devmasters.Config.GetWebConfigValue("ESConnection");
 
-            var singlePool = new Elasticsearch.Net.SingleNodeConnectionPool(new Uri(esUrl.Split(';').First()));
-/*            var pool = new Elasticsearch.Net.StaticConnectionPool(esUrl
+            // var singlePool = new Elasticsearch.Net.SingleNodeConnectionPool(new Uri(esUrl.Split(';').First()));
+            var pool = new Elasticsearch.Net.StaticConnectionPool(esUrl
                 .Split(';')
                 .Where(m => !string.IsNullOrWhiteSpace(m))
                 .Select(u => new Uri(u))
                 );
-*/
+
             //var pool = new Elasticsearch.Net.SniffingConnectionPool(esUrl
             //    .Split(';')
             //    .Where(m=>!string.IsNullOrWhiteSpace(m))
             //    .Select(u => new Uri(u)));
-            var settings = new ConnectionSettings(singlePool)
+            var settings = new ConnectionSettings(pool)
                 .DefaultIndex(indexName)
                 .DisableAutomaticProxyDetection(false)
                 .RequestTimeout(TimeSpan.FromMilliseconds(timeOut))
