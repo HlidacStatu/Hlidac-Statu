1a9491d03841058112158c0712ce1e8681ed1956
Michal Blaha <michal@michalblaha.cz>
2024-05-02 22:38:25 +0200

    ux changes

--- PlatyUredniku/Program.cs
+++ PlatyUredniku/Program.cs
@@ -139,7 +139,8 @@ public class Program
                 {
                     "wwwroot/Content/GlobalSite.v1.15.css",
                     "wwwroot/Content/social-share-kit.css",
-                    "wwwroot/Content/new.v1.15.css"
+                    "wwwroot/Content/new.v1.15.css",
+                    "wwwroot/css/site.css",
                 };
 
 
