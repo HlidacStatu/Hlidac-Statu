cc03ba2f32bc11536baf4d478dd523cc809cfb38
Michal Blaha <michal@michalblaha.cz>
2024-05-13 23:18:42 +0200

    chart improvements

--- PlatyUredniku/Models/AreaRangeCompareRenderOptions.cs
+++ PlatyUredniku/Models/AreaRangeCompareRenderOptions.cs
@@ -1,9 +1,11 @@
-﻿namespace PlatyUredniku.Models;
+﻿using System.Collections.Generic;
+
+namespace PlatyUredniku.Models;
 
 public class AreaRangeCompareRenderOptions
 {
-    public AreaRangePlot Data1 { get; set; }
-    public AreaRangePlot Data2 { get; set; }
+    public AreaRangePlot[] Data { get; set; }
+    //public AreaRangePlot Data2 { get; set; }
     public string CssWidth { get; set; } = "100%";
     public string CssHeight { get; set; } = "100%"; //$"{9 / 16 * 100}%'"; //16:9
     public string TextForNoData { get; set; } = string.Empty;
