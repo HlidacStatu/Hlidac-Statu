2a9182ef7cc132da5bbf76a4925d250708a75f2b
Michal Blaha <michal@michalblaha.cz>
2024-05-29 19:27:53 +0200

    ux

--- PlatyUredniku/Models/NamestciMinisterstevSummary.cs
+++ PlatyUredniku/Models/NamestciMinisterstevSummary.cs
@@ -5,7 +5,7 @@
             public int Rok { get; set; }
             public string NazevOrganizace { get; set; }
             public string DatovaSchranka { get; set; }
-            public decimal SouhrnPlatu { get; set; }
+            public decimal? SouhrnPlatu { get; set; }
         
     }
 }
