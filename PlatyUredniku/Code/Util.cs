43209dbeee50974ed4c1400b1f86a194e9b3d1b2
Michal Blaha <michal@michalblaha.cz>
2024-05-29 17:09:57 +0200

    ux

--- PlatyUredniku/Code/Util.cs
+++ PlatyUredniku/Code/Util.cs
@@ -1,4 +1,7 @@
-﻿namespace PlatyUredniku
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace PlatyUredniku
 {
     public static class Util
     {
@@ -6,5 +9,21 @@
         { 
         }
 
+        public static string GetFirstQueryParameter(this IQueryCollection queryCollection, string parameterName)
+        {
+            if (queryCollection.TryGetValue(parameterName, out var par))
+            {
+                return par.FirstOrDefault();
+            }
+            return string.Empty;
+        }
+        public static string[] GetQueryParameters(this IQueryCollection queryCollection, string parameterName)
+        {
+            if (queryCollection.TryGetValue(parameterName, out var par))
+            {
+                return par.Select(m=>m).ToArray();
+            }
+            return System.Array.Empty<string>();
+        }
     }
 }
