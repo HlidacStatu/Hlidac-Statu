bf39b5d000645da5418f970402f0903593c35af5
Suchoss <suchoss@gmail.com>
2024-05-30 16:30:07 +0200

    Extending ceos PrumerPlatu

--- PlatyUredniku/Models/PrumerPlatu.cs
+++ PlatyUredniku/Models/PrumerPlatu.cs
@@ -6,8 +6,11 @@ public class PrumerPlatu
     public string NazevOrganizace { get; set; }
     
     public decimal? PlatPrvniRok { get; set; }
+    public int PocetPlatuPrvniRok { get; set; }
+    public decimal? PocetMesicuPrvniRok { get; set; }
     public decimal? PlatPosledniRok { get; set; }
-    public decimal? PocetMesicu { get; set; }
+    public int PocetPlatuPosledniRok { get; set; }
+    public decimal? PocetMesicuPosledniRok { get; set; }
     public decimal? NarustAbsolutni => PlatPosledniRok - PlatPrvniRok;
     public decimal? NarustProcentualni => (PlatPosledniRok/PlatPrvniRok) - 1;    
 }
