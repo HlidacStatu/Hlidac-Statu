6031ad6a61fd59c88e2c6f217ee78b144d8fb90e
Suchoss <suchoss@gmail.com>
2024-06-14 08:56:25 +0200

    filter bad chars

--- Util/TextTools.cs
+++ Util/TextTools.cs
@@ -6,9 +6,12 @@ public static class TextTools
 {
     public static bool IsValidCharacter(char c)
     {
-        return (c >= 0x20 && c <= 0x7E) || (c >= 0xA0 && c <= 0xFF) || (c >= 0x100 && c <= 0x17F) ||
-               (c >= 0x400 && c <= 0x4FF) || (c >= 0x2000 && c <= 0x206F) || (c >= 0x2E00 && c <= 0x2E7F) ||
-               char.IsWhiteSpace(c);
+        return (c <= 0x2D67) &&  //odfiltrovat čínštinu,... 
+               (char.IsWhiteSpace(c) || 
+                char.IsSeparator(c) || 
+                char.IsPunctuation(c) || 
+                char.IsSymbol(c) ||
+                char.IsLetterOrDigit(c));
     }
     
     public static string CleanBadBytesFromText(this string text)
