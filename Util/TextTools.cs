67c5c22386b315ae5e4e850eac0a63a7d9157267
Suchoss <suchoss@gmail.com>
2024-06-19 12:29:25 +0200

    Improvement in finding binary characters

--- Util/TextTools.cs
+++ Util/TextTools.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Text;
 
 namespace HlidacStatu.Util;
 
 public static class TextTools
 {
+    [Obsolete("Use IsBadChar - this is copied from .net code and should be more reliable")]
     public static bool IsValidCharacter(char c)
     {
         return (c <= 0x2D67) &&  //odfiltrovat čínštinu,... 
@@ -14,12 +16,46 @@ public static class TextTools
                 char.IsLetterOrDigit(c));
     }
     
-    public static bool IsBadChar(char c)
+    /// <summary>
+    /// Detects if character is binary value => unusable for text 
+    /// </summary>
+    public static bool IsBadChar(char currentChar, char? nextChar )
     {
-        return (c > 0x2D67) || 
-               (c == 127) || //del
-               (c >= 0  && c <= 8);
+        if (currentChar >= '\uD800')
+        {
+            if (currentChar == '\uFFFE' || char.IsLowSurrogate(currentChar))
+                return true;
+            if (char.IsHighSurrogate(currentChar))
+            {
+                if (nextChar is null || !char.IsLowSurrogate(nextChar.Value))
+                    return true;
+            }
+        }
 
+        return false;
+    }
+    
+    /// <summary>
+    /// Detects if string contains binary value 
+    /// </summary>
+    public static bool HasInvalidUnicodeSequence(string s)
+    {
+        for (int index = 0; index < s.Length; ++index)
+        {
+            char c = s[index];
+            if (c >= '\uD800')
+            {
+                if (c == '\uFFFE' || char.IsLowSurrogate(c))
+                    return true;
+                if (char.IsHighSurrogate(c))
+                {
+                    if (index + 1 >= s.Length || !char.IsLowSurrogate(s[index + 1]))
+                        return true;
+                    ++index;
+                }
+            }
+        }
+        return false;
     }
     
     public static string CleanBadBytesFromText(this string text)
@@ -27,11 +63,14 @@ public static class TextTools
         text = text.Normalize(NormalizationForm.FormKC);
         StringBuilder cleaned = new StringBuilder();
 
-        foreach (char c in text)
+        for (var index = 0; index < text.Length; ++index)
         {
-            if (IsValidCharacter(c))
+            var currentChar = text[index];
+            var nextChar = index + 1 < text.Length ? text[index + 1] : (char?)null;
+
+            if (!IsBadChar(currentChar, nextChar))
             {
-                cleaned.Append(c);
+                cleaned.Append(currentChar);
             }
         }
 
