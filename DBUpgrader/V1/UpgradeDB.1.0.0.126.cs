f6f183cee6be4db021c5f630486c812186e64f18
Michal Blaha <michal@michalblaha.cz>
2024-11-19 16:31:09 +0100

    Create UpgradeDB.1.0.0.126.cs

---
+++ DBUpgrader/V1/UpgradeDB.1.0.0.126.cs
@@ -0,0 +1,23 @@
+﻿using Devmasters.DatabaseUpgrader;
+
+namespace HlidacStatu.DBUpgrades
+{
+    public static partial class DBUpgrader
+    {
+        private partial class UpgradeDB
+        {
+            [DatabaseUpgradeMethod("1.0.0.126")]
+            public static void Init_1_0_0_126(IDatabaseUpgrader du)
+            {
+                //du.AddColumnToTable("UsePlugin", "int", "UptimeServer", false);
+
+                string sql = @"
+ALTER TABLE dbo.UptimeServer ADD
+	UsePlugin int NOT NULL CONSTRAINT DF_UptimeServer_UsePlugin DEFAULT 1
+
+";
+                //du.RunDDLCommands(sql);
+            }
+        }
+    }
+}
