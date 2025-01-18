fa7171b788369d4e49d742da49297a2675ada8dc
Michal Blaha <michal@michalblaha.cz>
2025-01-18 21:33:48 +0100

    dotace kategorie update

---
+++ Entities/Entities/Subsidy.Hint.Category.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Devmasters.Enums;
+using Nest;
+
+namespace HlidacStatu.Entities;
+
+public partial class Subsidy
+{
+    public partial class Hint
+    {
+        public class Category
+        {
+            [Number]
+            public int TypeValue { get; set; }
+
+            [Number]
+            public decimal Probability { get; set; }
+
+            [Date]
+            public DateTime Created { get; set; }
+
+
+            public CalculatedCategories CalculatedCategory()
+            {
+                return (CalculatedCategories)TypeValue;
+            }
+
+            public string CalculatedCategoryDescription()
+            {
+                return CalculatedCategory().ToNiceDisplayName();
+            }
+        }
+        
+        #endregion
+    }
+}
