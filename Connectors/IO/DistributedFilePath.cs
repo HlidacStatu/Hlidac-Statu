e40ae9fc90f5adb9337fca33a453f27351c182d7
Michal Blaha <michal@michalblaha.cz>
2025-03-12 20:37:43 +0100

    Constructor root variable fix

--- Connectors/IO/DistributedFilePath.cs
+++ Connectors/IO/DistributedFilePath.cs
@@ -8,39 +8,40 @@ namespace HlidacStatu.Connectors.IO
          where T : class
     {
         int hashLen = 0;
-        string root = "";
+        string _root = "";
         protected Func<T, string> funcToGetId = null;
-        public DistributedFilePath(int hashLength, string root, Func<T, string> getId)
+        public DistributedFilePath(int hashLength, string rootPath, Func<T, string> getId)
         {
             hashLen = hashLength;
-            this.root = root.Trim();
+            this._root = rootPath.Trim();
+
 
             // Normalize all separators to the OS-specific one
             if (Path.DirectorySeparatorChar == '\\')
-                root = root.Replace('/', Path.DirectorySeparatorChar);
+                this._root = rootPath.Replace('/', Path.DirectorySeparatorChar);
             if (Path.DirectorySeparatorChar == '/')
-                root = root.Replace('\\', Path.DirectorySeparatorChar);
+                this._root = rootPath.Replace('\\', Path.DirectorySeparatorChar);
             
             // Ensure the path ends with a directory separator
-            if (!root.EndsWith(Path.DirectorySeparatorChar))
-                root += Path.DirectorySeparatorChar;
+            if (!this._root.EndsWith(Path.DirectorySeparatorChar))
+                this._root += Path.DirectorySeparatorChar;
             
             funcToGetId = getId;
         }
 
-        public string Root { get { return root; } }
+        public string Root { get { return _root; } }
 
         public void InitPaths()
         {
-            if (!System.IO.Directory.Exists(root))
+            if (!System.IO.Directory.Exists(_root))
             {
-                System.IO.Directory.CreateDirectory(root);
+                System.IO.Directory.CreateDirectory(_root);
 
                 for (int i = 0; i < Math.Pow(16, hashLen); i++)
                 {
                     string hash = i.ToString("X" + hashLen);
-                    if (!System.IO.Directory.Exists(root + hash))
-                        System.IO.Directory.CreateDirectory(root + hash);
+                    if (!System.IO.Directory.Exists(_root + hash))
+                        System.IO.Directory.CreateDirectory(_root + hash);
                 }
             }
         }
@@ -53,7 +54,7 @@ namespace HlidacStatu.Connectors.IO
         public virtual string GetFullDir(T obj)
         {
             return string.Format("{0}{1}{2}"
-                , root, GetHash(obj),Path.DirectorySeparatorChar);
+                , _root, GetHash(obj),Path.DirectorySeparatorChar);
         }
 
         public virtual string GetRelativeDir(T obj)
