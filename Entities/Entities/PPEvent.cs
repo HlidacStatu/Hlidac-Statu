2324917571d3ffaa0ada0f5ce48e00ff74057d08
Michal Blaha <michal@michalblaha.cz>
2025-04-09 11:27:33 +0200

    Create PPEvent.cs

---
+++ Entities/Entities/PPEvent.cs
@@ -0,0 +1,49 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HlidacStatu.Entities;
+
+[Table("PP_Event")]
+public class PPevent
+{
+    public enum TypUdalosti : int
+    {
+        Neurceno = 0,
+        ZaslaniZadosti = 1,
+        Upresneni = 2,
+        UplneOdmitnutiPoskytnutiInformaci = 3,
+        CastecneOdmitnutiPoskytnutiInformaci = 4,
+        PoskytnutiInformace = 5,
+        Zadost_o_UhraduNakladu = 6,
+        Stiznost = 7,
+        Odvolani = 8,
+        Jine = 9,
+    }
+
+    public enum SmerKomunikace : int
+    {
+        ObecnaUdalost = 0,
+        ZpravaOdNas = 1,
+        ZpravaProNas = 2
+    }
+
+    [Key]
+    [Column("pk")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Pk { get; set; }
+
+    public string OsobaNameId { get; set; }
+
+    public TypUdalosti Typ { get; set; } = 0;
+    public SmerKomunikace Smer { get; set; } = 0;
+
+    public int IcoOrganizace { get; set; }
+    public DateTime Datum { get; set; }
+
+    public string NaseCJ { get; set; }
+    public string Poznamka { get; set; }
+
+    public string PoznamkaSkryta { get; set; }
+
+}
