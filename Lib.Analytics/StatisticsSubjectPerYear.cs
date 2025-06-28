5263c1ed33e1415ffbd54fb91ed089df7180ee34
Michal Blaha <michal@michalblaha.cz>
2025-06-28 17:19:16 +0200

    Update StatisticsSubjectPerYear.cs

--- Lib.Analytics/StatisticsSubjectPerYear.cs
+++ Lib.Analytics/StatisticsSubjectPerYear.cs
@@ -5,12 +5,26 @@ using System.Text.Json.Serialization;
 
 namespace HlidacStatu.Lib.Analytics
 {
+    public class StatisticsPerSubjectComparer<T> : IEqualityComparer<StatisticsSubjectPerYear<T>>
+        where T : CoreStat, IAddable<T>, new()
+    {
+        public bool Equals(StatisticsSubjectPerYear<T> x, StatisticsSubjectPerYear<T> y)
+        {
+            return x?.ICO == y?.ICO;
+        }
+        public int GetHashCode(StatisticsSubjectPerYear<T> obj)
+        {
+            return obj?.ICO?.GetHashCode() ?? 0;
+        }
+    }
+
+
     [JsonConverter(typeof(StatisticsPerYearConverter))]
     public class StatisticsSubjectPerYear<T> : StatisticsPerYear<T>
         where T : CoreStat, IAddable<T>, new()
     {
         public string ICO { get; set; }
-        
+
         [JsonConstructor]
         public StatisticsSubjectPerYear()
         : base()
@@ -63,7 +77,7 @@ namespace HlidacStatu.Lib.Analytics
 
             return aggregatedStatistics;
         }
-        
+
 
     }
 }
