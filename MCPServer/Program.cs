5fde3fda83942e047abc0e75605930365a4fd566
Michal Blaha <michal@michalblaha.cz>
2025-07-23 19:13:37 +0200

    new nuget version

--- MCPServer/Program.cs
+++ MCPServer/Program.cs
@@ -72,6 +72,8 @@ _ = builder.Services.AddMcpServer(
     )
     .WithHttpTransport(opt =>
     {
+        //opt.PerSessionExecutionContext = true;
+
         opt.ConfigureSessionOptions = HlidacStatu.MCPServer.Code.Auth.ConfigureSessionCheckCookieAsync;
         //opt.RunSessionHandler = HlidacStatu.MCPServer.Code.Auth.RunSessionCheckCookieAsync;
     })
@@ -122,38 +124,4 @@ static bool IsDevelopment(IHostEnvironment hostEnvironment)
     return hostEnvironment.IsEnvironment("Petr") ||
            hostEnvironment.IsEnvironment("Michal") ||
            hostEnvironment.IsEnvironment("Development");
-}
-
-void AddIdentity(IServiceCollection services)
-{
-    services.AddDefaultIdentity<ApplicationUser>(options =>
-        {
-            options.SignIn.RequireConfirmedAccount = true;
-
-            options.User.RequireUniqueEmail = true;
-
-            options.Password.RequiredLength = 6;
-            options.Password.RequireNonAlphanumeric = false;
-            options.Password.RequireDigit = false;
-            options.Password.RequireLowercase = false;
-            options.Password.RequireUppercase = false;
-
-            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            options.Lockout.MaxFailedAccessAttempts = 5;
-        })
-        .AddRoles<IdentityRole>()
-        .AddEntityFrameworkStores<DbEntities>();
-
-    // this is needed because passwords are stored with old hashes
-    services.Configure<PasswordHasherOptions>(options =>
-        options.CompatibilityMode = PasswordHasherCompatibilityMode.IdentityV2
-    );
-
-    services.ConfigureApplicationCookie(o =>
-    {
-        o.Cookie.Domain = ".hlidacstatu.cz";
-        o.Cookie.Name = "HlidacLoginCookie"; // Name of cookie     
-
-        o.Cookie.SameSite = SameSiteMode.Lax;
-    });
 }
