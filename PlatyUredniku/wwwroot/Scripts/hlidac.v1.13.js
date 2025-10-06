258f561c8bd86ad5138c01592833c877788dd818
Suchoss <suchoss@gmail.com>
2025-10-06 15:55:59 +0200

    Remove redundant modal together with faulty script

--- PlatyUredniku/wwwroot/Scripts/hlidac.v1.13.js
+++ PlatyUredniku/wwwroot/Scripts/hlidac.v1.13.js
@@ -427,16 +427,6 @@ $(function () {
 
     Hamburger();
 
-    $("#VZInfoAnon").on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget); // Button that triggered the modal
-        var url = encodeURI(button.data('url')); // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this);
-        modal.find('#VZInfoAnonRegister').attr("href", "/account/Register?returnUrl=" + url);// text('New message to ' + recipient);
-        modal.find('#VZInfoAnonLogin').attr("href", "/account/Login?returnUrl=" + url);
-    })
-
 });
 
 // search functions below
