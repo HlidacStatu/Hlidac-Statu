4fd9f10009c0ef74db93c7f4ba6b71c551569f62
Suchoss <suchoss@gmail.com>
2025-11-14 10:04:39 +0100

    Startup tracking

--- Web/Program.cs
+++ Web/Program.cs
@@ -9,11 +9,16 @@ namespace HlidacStatu.Web
     {
         public static void Main(string[] args)
         {
+            StartupLogger.Write("Application started.");
             AppContext.SetSwitch("Switch.Microsoft.Data.SqlClient.UseManagedNetworkingOnWindows", true);
 
-
+            
             var builder = WebApplication.CreateBuilder(args);
+            StartupLogger.Write("Builder created.");
+            
             builder.ConfigureHostForWeb(args);
+            StartupLogger.Write("Configuration loaded and Logger initialized.");
+            
             builder.WebHost.UseStaticWebAssets();
             
 #if DEBUG
@@ -23,17 +28,26 @@ namespace HlidacStatu.Web
             
             //get IConfiguration
             var configuration = builder.Configuration;
+            StartupLogger.Write("Configuration 2 initialized.");
             
             //inicializace statických proměnných
             Devmasters.Config.Init(configuration);
+            StartupLogger.Write("Devmasters Init inited");
             System.Globalization.CultureInfo.DefaultThreadCurrentCulture = Util.Consts.czCulture;
             System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = Util.Consts.csCulture;
+            
+            
             DBUpgrades.DBUpgrader.UpgradeDatabases(Connectors.DirectDB.Instance.DefaultCnnStr);
+            StartupLogger.Write("Db upgrader upgraded.");
+            
             builder.Services.ConfigureServices(configuration);
-
-
+            StartupLogger.Write("Services configured.");
+            
             WebApplication app = builder.Build();
+            StartupLogger.Write("App builded");
+            
             app.ConfigurePipeline();
+            StartupLogger.Write("Pipeline configured");
             
             app.Run();
         }
