1acdf18bce01e455e267f3510a2a8ecca948190c
Michal Blaha <michal@michalblaha.cz>
2025-11-26 11:05:03 +0100

    webgenerator project

---
+++ WebGenerator/Models/ImageBannerCoreData.cs
@@ -0,0 +1,14 @@
+namespace HlidacStatu.WebGenerator.Models
+{
+    public class ImageBannerCoreData
+    {
+        public string title { get; set; }
+        public string subtitle { get; set; }
+        public string body { get; set; }
+        public string footer { get; set; }
+        public string img { get; set; }
+        public string color { get; set; }
+    }
+
+
+}
---
+++ WebGenerator/Program.cs
@@ -0,0 +1,42 @@
+using HlidacStatu.LibCore.Extensions;
+
+namespace Gen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.ConfigureHostForWeb(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{action=Index}/{id?}", 
+                defaults: new { controller = "Home" });
+
+            app.Run();
+        }
+    }
+}
