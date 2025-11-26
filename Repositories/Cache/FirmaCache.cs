62fed9778f0d814e474d5913b1547f7bad9cb0e9
Suchoss <suchoss@gmail.com>
2025-11-26 13:32:14 +0100

    Improve cache perf => change to valuetasks

--- Repositories/Cache/FirmaCache.cs
+++ Repositories/Cache/FirmaCache.cs
@@ -11,7 +11,7 @@ namespace HlidacStatu.Repositories.Cache;
 
 public static class FirmaCache
 {
-    private static readonly ILogger _logger = Log.ForContext(typeof(FirmaCache));
+    // private static readonly ILogger _logger = Log.ForContext(typeof(FirmaCache));
 
     private static readonly IFusionCache PostgreCache =
         Hlidacstatu.Caching.CacheFactory.CreateNew(CacheFactory.CacheType.L2PostgreSql, nameof(FirmaCache));
@@ -19,43 +19,27 @@ public static class FirmaCache
     private static readonly IFusionCache MemcachedCache =
         Hlidacstatu.Caching.CacheFactory.CreateNew(CacheFactory.CacheType.L2Memcache, nameof(FirmaCache));
 
-    public static async Task<InfoFact[]> GetInfoFactsAsync(Firma firma)
-    {
-        var infoFacts = await PostgreCache.GetOrSetAsync($"_InfoFacts:{firma.ICO}",
+    public static ValueTask<InfoFact[]> GetInfoFactsAsync(Firma firma) =>
+        PostgreCache.GetOrSetAsync($"_InfoFacts:{firma.ICO}",
             _ => FirmaExtension.GetDirectInfoFactsAsync(firma)
         );
-        return infoFacts;
-    }
 
-    public static async Task InvalidateInfoFactsAsync(Firma firma)
-    {
-        await PostgreCache.ExpireAsync($"_InfoFacts:{firma.ICO}");
-    }
+    public static ValueTask InvalidateInfoFactsAsync(Firma firma) => PostgreCache.ExpireAsync($"_InfoFacts:{firma.ICO}");
 
-    public static async Task<Riziko[]> GetRizikoAsync(Firma f, int rok)
-    {
-        var rizika = await PostgreCache.GetOrSetAsync($"_Rizika:{f.ICO}-{rok}",
+    public static ValueTask<Riziko[]> GetRizikoAsync(Firma f, int rok) =>
+        PostgreCache.GetOrSetAsync($"_Rizika:{f.ICO}-{rok}",
             _ => FirmaExtension.GetDirectRizikoAsync(f, rok)
         );
-        return rizika;
-    }
 
-    public static async Task InvalidateRizikoAsync(Firma f, int rok)
-    {
-        await PostgreCache.ExpireAsync($"_Rizika:{f.ICO}-{rok}");
-    }
+    public static ValueTask InvalidateRizikoAsync(Firma f, int rok) => PostgreCache.ExpireAsync($"_Rizika:{f.ICO}-{rok}");
 
-    public static async Task<Firma.Zatrideni.Item[]> GetSubjektyForOborAsync(Firma.Zatrideni.SubjektyObory obor)
-    {
-        var subjekty = await MemcachedCache.GetOrSetAsync($"_SubjektyForObor:{obor:G}",
+    public static ValueTask<Firma.Zatrideni.Item[]> GetSubjektyForOborAsync(Firma.Zatrideni.SubjektyObory obor) =>
+        MemcachedCache.GetOrSetAsync($"_SubjektyForObor:{obor:G}",
             _ => FirmaRepo.Zatrideni.GetSubjektyDirectAsync(obor)
         );
-        return subjekty;
-    }
 
-    public static FirmaRepo.Merk.MerkEnumConverters.CzechEnumsData GetMerkEnums()
-    {
-        var subjekty = MemcachedCache.GetOrSet($"_MerkEnums",
+    public static FirmaRepo.Merk.MerkEnumConverters.CzechEnumsData GetMerkEnums() =>
+        MemcachedCache.GetOrSet($"_MerkEnums",
             _ => FirmaRepo.Merk.MerkEnumConverters.GetMerkEnums(),
             options =>
             {
@@ -64,6 +48,4 @@ public static class FirmaCache
                 options.DistributedCacheFailSafeMaxDuration = TimeSpan.FromDays(1);
             } 
         );
-        return subjekty;
-    }
 }
