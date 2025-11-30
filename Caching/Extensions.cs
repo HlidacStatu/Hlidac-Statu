1efbc4808b4b2ad81d3d99cfca68df7652227272
Michal Blaha <michal@michalblaha.cz>
2025-11-30 14:58:05 +0100

    Refactor CacheFactory and add PersistentCacheProvider
    - Removed `ModifyEntryOptionsFactoryTimeouts` and `ModifyEntryOptionsDuration` from `CacheFactory`.
    + Added `Extensions` class in `Extensions.cs` with `ModifyEntryOptionsFactoryTimeouts` and `ModifyEntryOptionsDuration` methods.
    + Added `PersistentCacheProvider` class in `PersistentCacheProvider.cs` for saving and retrieving data in a persistent cache.
    * Updated `using` directives to include necessary namespaces.

---
+++ Caching/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZiggyCreatures.Caching.Fusion;
+
+namespace HlidacStatu.Caching
+{
+    public static class Extensions
+    {
+
+        public static FusionCacheEntryOptions ModifyEntryOptionsFactoryTimeouts(this FusionCacheEntryOptions options,
+            TimeSpan? factoryHardTimeout = null, TimeSpan? factorySoftTimeout = null)
+        {
+            if (factoryHardTimeout.HasValue)
+                options.FactoryHardTimeout = factoryHardTimeout.Value;
+
+            if (factorySoftTimeout.HasValue)
+                options.FactorySoftTimeout = factorySoftTimeout.Value;
+
+            return options;
+        }
+
+        public static FusionCacheEntryOptions ModifyEntryOptionsDuration(this FusionCacheEntryOptions options, TimeSpan mainDuration)
+        {
+            options.Duration = mainDuration;
+            options.FailSafeMaxDuration = mainDuration * 4;
+            options.DistributedCacheFailSafeMaxDuration = mainDuration * 4 * 4;
+            return options;
+        }
+    }
+}
