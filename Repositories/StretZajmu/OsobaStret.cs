0047cf7d740aefdaf17ec3b7d560148855573c54
Michal Blaha <michal@michalblaha.cz>
2026-02-06 00:16:35 +0100

    Refactor StretZajmu stats aggregation and UI display
    Added cached stats properties to OsobaStret for contracts and grants. Refactored StretZajmu.cshtml to use these properties, split persons into conflict/no-conflict groups, and improved UI with Bootstrap styling. Cleaned up redundant code in Michal.cs.

--- Repositories/StretZajmu/OsobaStret.cs
+++ Repositories/StretZajmu/OsobaStret.cs
@@ -19,6 +19,54 @@ namespace HlidacStatu.Repositories.StretZajmu
 
         public List<StretFirma> Strety { get; set; } = new();
 
+        decimal _stat_smlouvySum = -1;
+        public decimal Stat_SmlouvySum { 
+            get {
+                if (_stat_smlouvySum < 0)
+                {
+                    _stat_smlouvySum = Strety.Sum(s => s.SmlouvyStat.Summary().CelkovaHodnotaSmluv);
+                }
+                return _stat_smlouvySum;            
+            } 
+        }
+        decimal _stat_smlouvyNum = -1;
+        public decimal Stat_SmlouvyCount
+        {
+            get
+            {
+                if (_stat_smlouvyNum < 0)
+                {
+                    _stat_smlouvyNum = Strety.Sum(s => s.SmlouvyStat.Summary().PocetSmluv);
+                }
+                return _stat_smlouvyNum;
+            }
+        }
+        decimal _stat_dotaceSum = -1;
+        public decimal Stat_DotaceSum
+        {
+            get
+            {
+                if (_stat_dotaceSum < 0)
+                {
+                    _stat_dotaceSum = Strety.Sum(s => s.DotaceStat.Summary().CelkemPrideleno);
+                }
+                return _stat_dotaceSum;
+            }
+        }
+        decimal _stat_dotaceNum = -1;
+        public decimal Stat_DotaceCount
+        {
+            get
+            {
+                if (_stat_dotaceNum < 0)
+                {
+                    _stat_dotaceNum = Strety.Sum(s => s.DotaceStat.Summary().PocetDotaci);
+                }
+                return _stat_dotaceNum;
+            }
+        }
+
+
         public DateTime GeneratedAt { get; set; } = DateTime.Now;
     }
     public class StretFirma
